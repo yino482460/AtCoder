5cf04c3cc39b6a2a856b3ee9c8edb9eb7f6c65a4
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-11 16:04:01 +0900

    時刻を変換する処理を追加

--- D_Precipitation_Rain_Time.c
+++ D_Precipitation_Rain_Time.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N_max 60002
+#define N_max 30001
+
+int minutes_to_Time (int Time) {
+    Time = (Time/60)*100 + Time%60;
+    return Time;
+}
 
 int main(int argc, char const *argv[]) {
     //入力
@@ -11,11 +16,18 @@ int main(int argc, char const *argv[]) {
     //時刻の読み込み
     for (i = 0; i < N; i++) {
         scanf("%d-%d", &S[i], &E[i] );
+        //時刻を0:00からの経過分に換算
+        S[i] = ((S[i]/100)*60 + S[i]%100)/5*5;
+        E[i] = ((E[i]/100)*60 + S[i]%100 + 4)/5*5;
+        //換算した分を時刻に変換
+        S[i] = minutes_to_Time(S[i]);
+        E[i] = minutes_to_Time(E[i]);
     }
 
-    //出力
+
+    //確認用
     for ( i = 0; i < N; i++) {
-        printf("%d %d\n", S[i], E[i] );
+        printf("%04d %04d\n", S[i], E[i] );
     }
     return 0;
 }
