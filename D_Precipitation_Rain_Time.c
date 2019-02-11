67c217b7bbd791d19e35d03ecd3a7ef9a5d04a3e
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-11 17:51:00 +0900

    時刻を経過時間に変換する関数とimos法の追加

--- D_Precipitation_Rain_Time.c
+++ D_Precipitation_Rain_Time.c
@@ -4,21 +4,36 @@
 #define N_max 30001
 
 //時刻の切り下げ
-int Round_down_Time (int Time) {
+int Time_to_minutes_Round_dowm (int Time) {
     Time = ((Time/100)*60 + Time%100)/5*5;
     return Time;
 }
 //時刻の切り上げ
-int Round_up_Time (int Time) {
+int Time_to_minutes_Round_up (int Time) {
     Time = ((Time/100)*60 + Time%100 + 4)/5*5;;
     return Time;
 }
 
 //経過分を時刻に変換する関数
-int minutes_to_Time (int Time) {
-    Time = (Time/60)*100 + Time%60;
+int minutes_to_Time (int minutes) {
+    int Time;
+    Time = (minutes/60)*100 + minutes%60;
     return Time;
 }
+// 時刻を経過分に変換する関数
+int Time_to_minutes (int Time) {
+    int minutes;
+    minutes = (Time/100)*60 + Time%100;
+    return minutes;
+}
+
+// imos法 配列
+int imos[298] = {0};
+// imos法 実装
+void Imos(int Time) {
+    /* code */
+}
+
 
 int main(int argc, char const *argv[]) {
     //入力
@@ -28,10 +43,8 @@ int main(int argc, char const *argv[]) {
     //時刻の読み込み
     for (i = 0; i < N; i++) {
         scanf("%d-%d", &S[i], &E[i] );
-        //時刻の切り下げ、切り上げ
-        S[i] = Round_down_Time(S[i]);
-        E[i] = Round_up_Time(E[i]);
-        //換算した分を時刻に変換
+        S[i] = Time_to_minutes_Round_dowm(S[i]);
+        E[i] = Time_to_minutes_Round_up(E[i]);
         S[i] = minutes_to_Time(S[i]);
         E[i] = minutes_to_Time(E[i]);
     }
