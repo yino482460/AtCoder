3e73659a78adb70d3775a8aeaf4f105dee90f612
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-18 14:56:55 +0900

    D問題ほぼ完成(満点回答ではない)

--- D_Winter.c
+++ D_Winter.c
@@ -2,7 +2,8 @@
 #include <math.h>
 #define R_max 30
 #define C_max 30
-long tPascal[R_max*R_max+1][R_max*R_max+1] ;
+#define P_max R_max*R_max
+long long tPascal[P_max+1][P_max+1] ;
 
 int calc_combi_XY ( int R, int C, int X, int Y ) {
     int combiXY = 0;
@@ -10,15 +11,28 @@ int calc_combi_XY ( int R, int C, int X, int Y ) {
     return combiXY;
 }
 
-// Pascalの三角形を利用してnCrを計算
-void calc_nCr (int n, int r) {
+// 繰り返し2乗法
+long long nPow(int x, int y) {
+    if (y == 0) {
+        return 1;
+    } else if (y == 1) {
+        return x;
+    } else if (y%2 == 0) {
+        return nPow(x, y/2)*nPow(x, y/2);
+    } else {
+        return nPow(x, y/2)*nPow(x, y/2)*x;
+    }
+}
+
+// Pascalの三角形と余りの性質を利用してnCrの余りを計算
+void calc_nCr (int n, int r, long Mod) {
     tPascal[0][0] = 1;
     // 初期設定
     for (size_t i = 1; i <= n; i++) { tPascal[i][0] = 1; tPascal[i][i] = 1; }
     // 組み合わせの計算
     for (size_t i = 1; i <= n; i++) {
         for (size_t j = 1; j <= n; j++) {
-            tPascal[i][j] = tPascal[i-1][j-1] + tPascal[i-1][j];
+            tPascal[i][j] = tPascal[i-1][j-1]%Mod + tPascal[i-1][j]%Mod;
         }
     }
 }
@@ -33,14 +47,14 @@ int main(int argc, char const *argv[]) {
     scanf("%d %d", &X, &Y );
     scanf("%d %d", &D, &L );
     // X x Yスペースの配置の仕方
-    long ans;
+    long long ans;
+    long Mod = nPow(10, 9) + 7;
     int combiXY = calc_combi_XY(R, C, X, Y);
     // 机とサーバーラックの組み合わせを計算
-    calc_nCr(X*Y, L);
-    printf("tPascal %ld\n", tPascal[X*Y][D] );
-    ans = combiXY*tPascal[X*Y][D];
-    ans = ans%( (long)pow(10,9)+7 );
+    calc_nCr(X*Y, D, Mod); calc_nCr(X*Y-D, L, Mod);
+    ans = combiXY*(tPascal[X*Y][D]*tPascal[X*Y-D][L]);
+    ans = ans%Mod;
     // 出力
-    printf("%ld\n", ans );
+    printf("%lld\n", ans );
     return 0;
 }
