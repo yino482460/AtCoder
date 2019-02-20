69c872f81af821477515cd3a0b29c8058e337009
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-20 18:18:24 +0900

    部分点40点回答

--- D_Original.c
+++ D_Original.c
@@ -4,8 +4,8 @@
 #define Infinity 10000000
 #define Left_min -300
 #define Right_max 300
-#define min(a, b) (a<b ? a:b)
-#define max(a, b) (a>b ? a:b)
+#define min(a, b) (a<=b ? a:b)
+#define max(a, b) (a>=b ? a:b)
 // 変数
 int R, G, B;
 int sum;
@@ -13,10 +13,10 @@ int Base[3] = {-100, 0, 100};   // R:0 G:1 B:2
 // マーブルの移動コスト
 long Move_Cost (int left, int base, int marbles) {
     int cost = 0;
-    int right = left + marbles;    // 右側の座標
+    int right = left + marbles - 1;    // 右側の座標
     // 最小移動手数
     cost += 0.5*abs(left-base)*abs(left-base-1);  // 左側
-    cost += 0.5*abs(right-base)*abs(right-base-1);   // 右側
+    cost += 0.5*abs(right-base)*abs(right-base+1);   // 右側
     return cost;
 }
 // マーブルの最小移動コスト
@@ -24,7 +24,7 @@ long min_Move_Cost (int R, int G, int B) {
     long min_cost = Infinity;
     // 各石の開始位置
     int R_right = Base[0] + R/2;
-    int  G_left;
+    int  G_left, G_right;
     int B_left = Base[2] - B/2;
     // 各石の配置コスト
     long R_cost, G_cost, B_cost, sum_cost;
@@ -33,11 +33,12 @@ long min_Move_Cost (int R, int G, int B) {
     for (G_left = Left_min; G_left < Right_max; G_left++) {
         // Rの開始位置
         r = min(R_right, G_left);   // Rの右端を計算
-        r = r - R +1 ;  // Rの左端を計算
+        r = r-R+1;   // Rの左端の計算
         // Gの開始位置
         g = G_left;
+        G_right = g+G;
         // Bの開始位置
-        b = max(g+G+1, B_left);
+        b = max(G_right, B_left);
         // 各コストの計算
         R_cost = Move_Cost(r, Base[0], R);
         G_cost = Move_Cost(g, Base[1], G);
