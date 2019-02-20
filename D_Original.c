0a4c065805c98f16a7cbd31a144c6f8c569c542e
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-20 11:05:32 +0900

    最小移動コストを計算する関数のデバッグ開始

--- D_Original.c
+++ D_Original.c
@@ -11,9 +11,9 @@ int Base[3] = {-100, 0, 100};
 // マーブルの移動コストを計算する関数
 long Move_Cost (int left, int base, int marbles) {
     int cost = 0;
-    int right = base + marbles - abs(base-left);    // 右側の座標
+    int right = marbles + left;    // 右側の座標
     // 最小移動手数
-    cost = 0.5*abs(left-base)*abs(left-base-1);  // 左側
+    cost += 0.5*abs(left-base)*abs(left-base-1);  // 左側
     cost += 0.5*abs(right-base)*abs(right-base-1);   // 右側
     return cost;
 }
@@ -28,33 +28,37 @@ long calc_min_Move_Cost (int R, int G, int B) {
     int R_right = R/2 + Base[0];
     int R_left = Base[0] - R/2;
     long  R_cost = Move_Cost(R_left ,Base[0], R);
-    if (R_right >= G_left) { // 赤と緑の配置が被る場合の探索
+    long num = 0;
+    if (G_left <= R_right) { // 赤と緑の配置が被る場合の探索
         // 限界まで左に寄せる
         R_left = Left_min; G_left = R_left+R+1;
-        for (int i = Base[1]; i < Base[2]; i++) {
-            for (int j = Left_min; j < Base[0]; j++) {
-                R_left++; G_left++;
+        for (int i = 0; i < 300; i++) {
+                R_left++; G_left++; // 右に動かす
                 R_cost = Move_Cost(R_left, Base[0], R);
                 G_cost = Move_Cost(G_left, Base[1], G);
-                if ((R_cost+G_cost) < min_cost) {   // 更新
+                num = R_cost + G_cost;
+                if ( num < min_cost && num>=0 ) {   // 更新
                     min_cost = R_cost+G_cost;
-                    G_right = G + G_left;
+                    //printf("G_left %d\n", G_left );
+                    G_right = G_left + G ;
+                    printf("G_right %d\n", G_right );
                 }
-            }
         }
     } else {
-        min_cost = R_cost+G_cost;
+        min_cost = R_cost + G_cost;
     }
+    printf("R+G min_cost %ld\n", min_cost );
     // 青の配置を考慮
     int B_left = Base[2] - B/2;
     long  B_cost = 0;
-    if (G_right >= B_left) { // 青と緑の配置が被る場合の探索
-        B_cost = Move_Cost(G_right+1 ,Base[2], B);
+    if (B_left <= G_right) { // 青と緑の配置が被る場合の探索
+        B_left = G_right + 1;
+        B_cost = Move_Cost(B_left ,Base[2], B);
+        printf("B_cost %ld\n", B_cost );
     } else {
         B_cost = Move_Cost(B_left ,Base[2], B);
     }
     min_cost += B_cost;
-
     return min_cost;
 }
 
