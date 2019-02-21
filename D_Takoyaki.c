74474d9813a7177feca8863da76a7cfd4e8f7bbe
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-21 17:20:38 +0900

    とりあえず完成

--- D_Takoyaki.c
+++ D_Takoyaki.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#define max(a,b) (a>=b ? a:b)
 #define N_max 51
 #define Q_max (N_max*N_max)
 int Taste[N_max][N_max];
-int Taste_sum[N_max][N_max];
+int Taste_right[N_max][N_max];
+int Taste_any[N_max][N_max][N_max][N_max];
+int Area_max[Q_max];
 // 変数
 int N;
 int Q, P[Q_max];    // 店員の人数
@@ -13,16 +16,16 @@ int sum_right (int i, int j) {
     if (i>N || j>N) {
         return 0;
     } else if (i==N && j==N) {
-        Taste_sum[N][N] = Taste[N][N];
-        return Taste_sum[N][N];
-    } else if (Taste_sum[i][j] != 0) {  // メモ化
-        return Taste_sum[i][j];
+        Taste_right[N][N] = Taste[N][N];
+        return Taste_right[N][N];
+    } else if (Taste_right[i][j] != 0) {  // メモ化
+        return Taste_right[i][j];
     } else {
-        Taste_sum[i][j] = sum_right(i+1, j) + sum_right(i, j+1);
-        Taste_sum[i][j] -=  sum_right(i+1, j+1);
-        Taste_sum[i][j] += Taste[i][j];
+        Taste_right[i][j] = sum_right(i+1, j) + sum_right(i, j+1);
+        Taste_right[i][j] -=  sum_right(i+1, j+1);
+        Taste_right[i][j] += Taste[i][j];
     }
-    return Taste_sum[i][j];
+    return Taste_right[i][j];
 }
 // 右下までの各大きさの長方形を計算
 void calc_all_sum (int N) {
@@ -32,6 +35,37 @@ void calc_all_sum (int N) {
         }
     }
 }
+// 任意の長方形の領域での美味しさを計算
+int calc_any_Rectangle (int i, int j, int v, int h) {
+    int any_taste_sum = 0;
+    any_taste_sum = Taste_right[i][j] - Taste_right[i+v][j] - Taste_right[i][j+h] + Taste_right[i+v][j+h];
+    Taste_any[i][j][v][h] = any_taste_sum;
+
+    return Taste_any[i][j][v][h];
+}
+// 全ての任意の長方形での美味しさの合計を計算
+void calc_all_Rectangle (int N) {
+    int area = 0;
+    for (int i = 1; i <= N; i++) {
+        for (size_t j = 1; j <= N; j++) {
+            for (size_t v = 1; v <= N-i; v++) {
+                for (size_t h = 1; h <= N-j; h++) {
+                    area = v*h;
+                    Area_max[area] = max(Area_max[area], calc_any_Rectangle(i, j, v, h));
+                }
+            }
+        }
+    }
+
+}
+// 全ての面積で最大値を再計算
+void calc_total_Taste (int all_rectangle) {
+    for (size_t i = 1; i <= all_rectangle; i++) {
+        Area_max[i] = max(Area_max[i-1], Area_max[i]);
+    }
+}
+
+
 
 int main(int argc, char const *argv[]) {
     // 美味しさの入力
@@ -47,12 +81,12 @@ int main(int argc, char const *argv[]) {
         scanf("%d", &P[i] );
     }
     // Taste_sumを0で初期化
-    memset(Taste_sum, 0, sizeof(Taste_sum));
+    memset(Taste_right, 0, sizeof(Taste_right));
     calc_all_sum(N);
     // 右下までの和
     for (size_t i = 0; i <= N; i++) {
         for (size_t j = 0; j <= N; j++) {
-            printf("%2d ", Taste_sum[i][j]);
+            printf("%2d ", Taste_right[i][j]);
         }
         printf("\n");
     }
