52cd359d200747f17a6beceedba48fe5fa3f0f23
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-21 23:48:42 +0900

    C問題完成

--- C_riddle.c
+++ C_riddle.c
@@ -4,16 +4,18 @@ long ans[3] = {-1, -1, -1};
 // つるかめ算
 int solve_equation(long population, int Legs) {
     int adults, olds, babys, legs;
-    for (adults = 0; adults <= population; adults++) {
-        for (olds = 0; olds <= 1; olds++) {
+    for (olds = 0; olds <= 1; olds++) {
+        for (adults = 0; adults <= population-olds; adults++) {
             babys = population-(adults+olds);
             legs = 2*adults + 3*olds + 4*babys;
             if (legs == Legs) {
                 ans[0] = adults; ans[1] = olds; ans[2] = babys;
+                printf("%ld %ld %ld\n", ans[0], ans[1], ans[2] );
                 return 0;   // 成功
             }
         }
     }
+    printf("%ld %ld %ld\n", ans[0], ans[1], ans[2] );
     return -1;  // 失敗
 }
 
@@ -26,10 +28,6 @@ int main(int argc, char const *argv[]) {
     scanf("%ld", &population );
     scanf("%ld", &legs );
     // 出力
-    if (solve_equation(population, legs)) {
-        printf("%ld %ld %ld\n", ans[0], ans[1], ans[2] );
-    } else {
-        printf("%ld %ld %ld\n", ans[0], ans[1], ans[2] );
-    }
+    solve_equation(population, legs);
     return 0;
 }
