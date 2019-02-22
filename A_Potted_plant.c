ed1e48d3177f2f21c6cbeb8aabe07edf9e727da0
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-22 17:08:52 +0900

    ABC007 A問題を作成

---
+++ A_Potted_plant.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    int trees, gap;
+    // 入力
+    scanf("%d", &trees );
+    // 隙間
+    gap = trees-1;
+    // 出力
+    printf("%d\n", gap );
+    return 0;
+}
