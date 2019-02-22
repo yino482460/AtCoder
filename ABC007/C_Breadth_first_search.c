ddd64ae35993e2529601917a089299df9a7edc78
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-23 01:51:09 +0900

    セグフォ

--- ABC007/C_Breadth_first_search.c
+++ ABC007/C_Breadth_first_search.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #define SIZE 52
-#define Wall 100
+#define Wall 10000
 int Board[SIZE][SIZE];
+int vx[4] = {0, 1, 0, -1};
+int vy[4] = {1, 0, -1, 0};
 
 // 盤面を数字に変換
 void  trans_to_num (int R, int C) {
@@ -15,6 +17,19 @@ void  trans_to_num (int R, int C) {
         }
     }
 }
+// 迷路を攻略
+int maze_solve (int startx, int starty) {
+    for (size_t i = 0; i < 4; i++) {
+        int nx = startx + vx[i];
+        int ny = starty + vy[i];
+        if (Board[nx][ny]!=Wall || Board[nx][ny]==0) {
+            Board[nx][ny] = Board[startx][starty] + 1;
+            maze_solve (nx, ny);
+        }
+    }
+    return -1;  // 行き止まり
+}
+
 
 int main(int argc, char const *argv[]) {
     // 変数
@@ -25,7 +40,10 @@ int main(int argc, char const *argv[]) {
     scanf("%d%d%d%d", &sy, &sx, &gy, &gx);
     // 文字列を数字に変換
     trans_to_num(R, C);
+    // 迷路の計算
+    maze_solve(sx, sy);
     // 出力
+    printf("%d\n", Board[gx][gy] );
 
 
     return 0;
