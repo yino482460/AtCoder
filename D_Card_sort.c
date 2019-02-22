4424ee47cde66599295a474e6d6799912032fedf
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-22 16:42:02 +0900

    最長増加数列の実装完了。D問題完成

--- D_Card_sort.c
+++ D_Card_sort.c
@@ -2,23 +2,23 @@
 #include <string.h>
 #define N_max 30002
 #define Infinity 100000
-long C[N_max];
-long DP[N_max];
+int C[N_max];
+int DP[N_max];
 // 最長増加数列の長さを計算
-int calc_LIS (int N, long C[]) {
+int calc_LIS (int N, int C[]) {
     DP[0] = -Infinity;
     // 配列の初期化
     for (size_t i = 1; i <= N; i++) {DP[i] = Infinity;}
-    // 最長増加数列
+
     int length = 0; // 数列の長さ
     int left, right, mid;
-
+    // 最長増加数列
     for (int i = 1; i <= N; i++) {
         if (C[i] > DP[length]) {
             DP[length+1] = C[i];
             length ++;  // 数列の長さの更新
         } else {    // 二分検索
-            left = 1; right = length; mid = (left+right)/2;
+            left = 0; right = length; mid = (left+right)/2;
             while (left < right-1) {
                 if (C[i] < DP[mid]) {
                     right = mid;
@@ -40,23 +40,13 @@ int main(int argc, char const *argv[]) {
     scanf("%d", &N );
     // 入力
     for (size_t i = 1; i <= N; i++) {
-        scanf("%ld", &C[i] );
+        scanf("%d", &C[i] );
     }
-
+    // 最長部分数列の長さの計算
     calc_LIS(N, C);
-    // 確認
-    printf("\n");
-    printf("DP[0] %ld\n", DP[0] );
-    for (size_t i = 1; i <= N; i++) {
-        printf("%2ld", DP[i] );
-    }
-    printf("\n");
-    // 計算
-    /*
     int ans;
     ans = N-calc_LIS(N, C);
     // 出力
     printf("%d\n", ans );
-    */
     return 0;
 }
