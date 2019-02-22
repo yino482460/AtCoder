4f8bf11249141e16cee1bedcd75f672da9df1b7b
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-22 22:25:27 +0900

    文字列変換部を関数化

--- ABC007/C_Breadth_first_search.c
+++ ABC007/C_Breadth_first_search.c
@@ -2,9 +2,18 @@
 #define SIZE 52
 #define Wall 100
 int Board[SIZE][SIZE];
+
 // 盤面を数字に変換
-void  trans_to_num (/* arguments */) {
-    /* code */
+void  trans_to_num (int R, int C) {
+    for (size_t i = 1; i <= R; i++) {
+        for (size_t j = 0; j <= C; j++) {
+            if (getchar() == '#') {
+                Board[i][j] = Wall;
+            } else {
+                Board[i][j] = 0;
+            }
+        }
+    }
 }
 
 int main(int argc, char const *argv[]) {
@@ -15,22 +24,9 @@ int main(int argc, char const *argv[]) {
     scanf("%d%d", &R, &C );
     scanf("%d%d%d%d", &sy, &sx, &gy, &gx);
     // 文字列を数字に変換
-    for (size_t i = 1; i <= R; i++) {
-        for (size_t j = 0; j <= C; j++) {
-            if (getchar() == '#') {
-                Board[i][j] = Wall;
-            } else {
-                Board[i][j] = 0;
-            }
-        }
-    }
-    // 確認
-    for (int i = 1; i <= R; i++) {
-        for (int j = 1; j <= C; j++) {
-            printf("%4d", Board[i][j] );
-        }
-        printf("\n");
-    }
+    trans_to_num(R, C);
+    // 出力
+
 
     return 0;
 }
