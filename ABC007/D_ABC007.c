7f265e6681cd5be7f03151fd3eb96b6a8396fbf8
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-02-24 09:28:11 +0900

    D問題作成開始

---
+++ ABC007/D_ABC007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define Max_digit 18
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    char A[Max_digit+1], B[Max_digit+1];
+    // 入力
+    scanf("%s %s", A, B );
+
+    // 確認
+    printf("%s\n", A );
+    printf("%s\n", B );
+    return 0;
+}
