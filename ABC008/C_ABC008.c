43983cb135f789d76e54b65a9f4c0ebc72f638b5
yino48246 <yino48246@gmail.com>
2019-02-26 22:28:04 +0900

    総当たり関数のループを作成

--- ABC008/C_ABC008.c
+++ ABC008/C_ABC008.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #define N_max 100
-int N;
 // 期待値の計算
-double all_Search (int num[]) {
+double all_Search (int N, int num[]) {
     // 階乗の計算
     long factorial = 1;
     for (int i = 1; i <= N; i++) {
         factorial = factorial*i;
     }
     // 総当たり
+    int here = 0;
+    int count = 0;  // 条件を満たす個数
+    int look = here+1;
+    while (look == N) {
+        if (num[look]%num[here] == 0) {
+            count++;
+        }
+        look++;
+    }
 
     double expected_value = 0;
-
-
+    expected_value = count/factorial;
     return expected_value;
 }
 
 int main(int argc, char const *argv[]) {
     // 変数
     int C[N_max];
+    int N;
     // 入力
     scanf("%d", &N );
     for (size_t i = 0; i < N; i++) {
