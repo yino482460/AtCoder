2c1243d3df8a1c17aca7bcd161bbb1c6efa43674
yino48246 <yino48246@gmail.com>
2019-02-26 22:08:08 +0900

    Update C_ABC008.c
    総当たり探索を実装開始

--- ABC008/C_ABC008.c
+++ ABC008/C_ABC008.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #define N_max 100
+int N;
+// 期待値の計算
+double all_Search (int num[]) {
+    // 階乗の計算
+    long factorial = 1;
+    for (int i = 1; i <= N; i++) {
+        factorial = factorial*i;
+    }
+    // 総当たり
+
+    double expected_value = 0;
+
+
+    return expected_value;
+}
 
 int main(int argc, char const *argv[]) {
     // 変数
-    int N;
-    long C[N_max];
+    int C[N_max];
     // 入力
     scanf("%d", &N );
     for (size_t i = 0; i < N; i++) {
-        scanf("%ld", &C[i] );
+        scanf("%d", &C[i] );
     }
     return 0;
 }
