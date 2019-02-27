e6937a05ea415d89425a7a4d612c4fbe32a02c3a
yino48246 <yino48246@gmail.com>
2019-02-27 13:54:03 +0900

    金塊を回収してその個数を返す関数を実装

--- ABC008/D_ABC008.c
+++ ABC008/D_ABC008.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #define gold 1
-#define Machine -1
+#define machine -1
 #define Max 1000001
+#define max(a, b) (a>b ? a:b)
 int Board[Max][Max];
 // 金塊と装置を盤面に配置
-void setGolds (int W, int H, int N, int machine[][2]) {
+void setGolds (int W, int H, int N, int location[][2]) {
     // 金塊の配置
     for (size_t i = 1; i <= H; i++) {
         for (size_t j = 1; j <= W; j++) {
@@ -13,25 +14,39 @@ void setGolds (int W, int H, int N, int machine[][2]) {
     }
     // クレーンを配置
     for (size_t i = 0; i < N; i++) {
-        int x = machine[i][0];
-        int y = H+1-machine[i][1];
-        Board[y][x] = Machine;
+        int x = location[i][0];
+        int y = location[i][1];
+        Board[y][x] = machine;
+    }
+}
+
+// 金塊の回収
+int solve (int x, int y) {
+    int ans = 0;
+    while (Board[y+1][x] !=0) {
+        Board[y+1][x] = 0;
+        y++;
+        ans++;
     }
 
+
+    return ans;
 }
 
 int main(int argc, char const *argv[]) {
     // 変数
     int W, H, N;
     scanf("%d %d %d", &W, &H, &N );
-    int machine[N+1][2];    // 回収装置の座標
+    int location[N+1][2];    // 回収装置の座標
     for (size_t i = 0; i < N; i++) {
         for (size_t j = 0; j <= 1; j++) {
-            scanf("%d", &machine[i][j] );
+            scanf("%d", &location[i][j] );
         }
     }
     // 盤面の設定
-    setGolds(W, H, N, machine);
+    setGolds(W, H, N, location);
+    // 金塊の回収
+    solve(location[1][0], location[1][1]);
     // 確認
     for (size_t i = 0; i <= H; i++) {
         for (size_t j = 0; j <= W; j++) {
@@ -39,6 +54,5 @@ int main(int argc, char const *argv[]) {
         }
         printf("\n");
     }
-
     return 0;
 }
