d11c1ecf3d0dd8924eb4d1ad0cfdf04076cb5472
yino48246 <yino48246@gmail.com>
2019-02-27 13:02:58 +0900

    盤面に金塊とクレーンを配置する関数を実装

--- ABC008/D_ABC008.c
+++ ABC008/D_ABC008.c
@@ -4,17 +4,18 @@
 #define Max 1000001
 int Board[Max][Max];
 // 金塊と装置を盤面に配置
-void setGolds (int W, int H, int machine[][]) {
-    int No = 0;
+void setGolds (int W, int H, int N, int machine[][2]) {
+    // 金塊の配置
     for (size_t i = 1; i <= H; i++) {
         for (size_t j = 1; j <= W; j++) {
-            if (machine[No][0]==W && machine[No][1] == H) {
-                Board[i][j] = Machine;
-                No++;
-            } else {
-                Board[i][j] = gold;
-            }
+            Board[i][j] = gold;
         }
+    }
+    // クレーンを配置
+    for (size_t i = 0; i < N; i++) {
+        int x = machine[i][0];
+        int y = H+1-machine[i][1];
+        Board[y][x] = Machine;
     }
 
 }
@@ -28,6 +29,15 @@ int main(int argc, char const *argv[]) {
         for (size_t j = 0; j <= 1; j++) {
             scanf("%d", &machine[i][j] );
         }
+    }
+    // 盤面の設定
+    setGolds(W, H, N, machine);
+    // 確認
+    for (size_t i = 0; i <= H; i++) {
+        for (size_t j = 0; j <= W; j++) {
+            printf("%2d", Board[i][j] );
+        }
+        printf("\n");
     }
 
     return 0;
