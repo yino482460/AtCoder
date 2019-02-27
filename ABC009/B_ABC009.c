b5f2f2b125e71fb8e00db4cdcd3148395cb7e7ac
yino48246 <yino48246@gmail.com>
2019-02-28 00:34:03 +0900

    B問題完成

---
+++ ABC009/B_ABC009.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N_max 100
+// Swap
+void Swap (int x[],int a, int b) {
+    int tmp;
+    tmp = x[a]; x[a] = x[b]; x[b] = tmp;
+}
+// Qsort
+void Qsort (int array[], int left, int right) {
+    int i, j;
+    int pivot;
+    i = left; j = right;
+    pivot = array[(i+j)/2];
+
+    while (1) {
+        while (array[i] > pivot) { i++; }
+        while (pivot > array[j]) { j--; }
+        if (i >= j) {
+            break;
+        }
+        Swap(array, i, j);
+        i++;  j--;
+    }
+    if (left < i-1) {   // 左に二つ以上の要素があればソート
+        Qsort( array, left, i-1 );
+    }
+    if (j+1 < right) {
+        Qsort( array, j+1, right );
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    int N;
+    int A[N_max];
+    // 入力
+    scanf("%d", &N );
+    for (size_t i = 0; i < N; i++) {
+        scanf("%d", &A[i] );
+    }
+    // ソート
+    Qsort(A, 0, N-1);
+    // 2番目に大きいものを探索
+    int No = 0;
+    while (No <= N-1) {
+        if (A[0] != A[No]) {
+            printf("%d\n", A[No] );
+            break;
+        }
+        No ++;
+    }
+    return 0;
+}
