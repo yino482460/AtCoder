946fff0392d33461152c8f43b656aba0d1fcb90a
yino48246 <yino48246@gmail.com>
2019-03-02 21:49:28 +0900

    文字列の並び替えを実装

--- ABC009/C_ABC009.c
+++ ABC009/C_ABC009.c
@@ -2,11 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #define string_MAX 101
+#define min(a, b) (a<b ? a:b)
 
 void sort_String (int N, int K, char str[]) {
     char T[N+1];
     int count_s[26] = {0}, count_t[26] = {0};
-    // 文字列に使われているアルファベットの数を計算
+    // 文字列に出てくるアルファベットの数を計算
     for (size_t i = 0; i < N; i++) {
         count_s[str[i] - 'a'] ++;
         count_t[str[i] - 'a'] ++;
@@ -14,11 +15,29 @@ void sort_String (int N, int K, char str[]) {
     // 並び替え
     for (size_t i = 0; i < N; i++) {
         for (size_t index = 0; index < 26; index++) {
-            if (count_t[i] == 0) { continue;} //含まれていないなら無視
-            
+            if (count_t[index] == 0) { continue;} //含まれていないなら無視
+            char c = 'a' + index;
+            int different = 0;  // 不一致の数
+            for (size_t j = 0; j < i; j++) {
+                if (str[j] != T[j]) {different ++;} // i-1文字目までの不一致の数
+            }
+            if (c != str[i]) {different ++;}    // i文字目で不一致かどうか
+            count_s[str[i] - 'a'] --;
+            count_t[index] --;
+            int number = (N-1)-i;   // 残りの文字数
+            for (size_t j = 0; j < 26; j++) {
+                number-=min(count_s[j],count_t[j]);
+            }
+            different += number;
+            if (different <= K) {
+                T[i] += ('a'+index);
+                break;
+            }
+            ++count_s[str[i]-'a'];
+			++count_t[index];
         }
     }
-
+    printf("%s\n", T );
 }
 
 int main(int argc, char const *argv[]) {
@@ -28,6 +47,7 @@ int main(int argc, char const *argv[]) {
     // 入力
     scanf("%d %d", &N, &K);
     scanf("%s", S);
-
+    // 出力
+    sort_String(N, K, S);
     return 0;
 }
