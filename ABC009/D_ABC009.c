a4c9e68ca9fa2fbc3b3562b2af712e7815a8870e
yino48246 <yino48246@gmail.com>
2019-03-03 14:21:10 +0900

    行列の階乗を計算する関数を実装

--- ABC009/D_ABC009.c
+++ ABC009/D_ABC009.c
@@ -2,33 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #define K_max 100
-// 行列とベクトルのセット
-unsigned int Matrix[K_max][K_max];
-unsigned int Result[K_max];
-unsigned int Vector[K_max];
 // 行列の用意
-void setMatrix (int k, unsigned int C[]) {
-    // 行列の初期化
-    memset(Matrix, 0, sizeof(Matrix));
+void setMatrix (int k, unsigned int C[], unsigned int Mat[][]) {
     // 行列の設定
     for (size_t i = 0; i < k; i++) {
-        Matrix[0][i] = C[i];    // 一行目
-        if (i>=1) {Matrix[i][i-1] = 1;}    // 対角線を埋める
+        Mat[0][i] = C[i];    // 一行目
+        if (i>=1) {Mat[i][i-1] = 1;}    // 対角線を埋める
     }
 }
 // 行列とベクトルのAND計算
 void mulMatrixVector (int k, unsigned int Matrix[][], unsigned int Vector[]) {
-    // Result の初期化
-    memset(Result, 0, sizeof(Result));
     // 行列とベクトルのANDの計算
     for (size_t i = 0; i < k; i++) {
         for (size_t j = 0; j < k; j++) {
-            Result[i] ^= Matrix[i][j]&Vector[j];
+            Vector[i] ^= Matrix[i][j]&Vector[j];
         }
     }
 }
 // 行列同士の積
-void malMatrix (unsigned int A[][], unsigned int B[][], int n) {
+void mulMatrix (unsigned int A[][], unsigned int B[][], int n) {
     for (size_t i = 0; i < n; i++) {
         for (size_t j = 0; j < n; j++) {
             for (size_t k = 0; k < n; k++) {
@@ -38,8 +30,21 @@ void malMatrix (unsigned int A[][], unsigned int B[][], int n) {
     }
 }
 // 行列の階乗
-void powMatrix (unsigned int Matrix[][], int m, int k) {
-    /* code */
+unsigned int powMatrix (unsigned int C[],int k, int n) {
+    unsigned int Matrix[K_max][K_max];
+    unsigned int ans;
+    // 行列の設定
+    memset(Matrix, 0, sizeof(Matrix));  // 全部を0に設定
+    setMatrix(k, C, Matrix);    // 漸化式の行列を設定
+    if (n==0) {
+        mulMatrixVector(k, Matrix, C);
+        ans = C[0];
+        return ans;
+    } else if (n%2 == 0) {
+        /* code */
+    } else {
+        /* code */
+    }
 }
 
 int main(int argc, char const *argv[]) {
