130df0290f831273b31e7c7fcf999fac719c74e5
yino48246 <yino48246@gmail.com>
2019-03-04 21:25:40 +0900

    パスカルの三角形を改良

--- ABC011/D_ABC011.c
+++ ABC011/D_ABC011.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 // 確率版コンビネーションを計算
-double Combi (int n, int r) {
-    double ans = 0;
+double Combi (long n, long r) {
     double combi[n+5][n+5];
-    combi[1][1] = 1;
-    for (size_t i = 2; i <= n; i++) {
-        combi[i][1] = combi[i-1][1]/2;
-        combi[i][i] = combi[i-1][1-1]/2;
+    // 初期設定
+    for (size_t i = 0; i <= n; i++) {
+        combi[0][i] = 1;
+        combi[i][0] = 0;
     }
+    // 組み合わせの計算
     for (size_t i = 1; i <= n; i++) {
-        for (size_t j = 1; j <= n; j++) {
+        for (size_t j = 1; j <= i; j++) {
             combi[i][j] = (combi[i-1][j-1]+combi[i-1][j])/2;
         }
     }
-
+    double ans = combi[n][r];
     return ans;
 }
 
 // 各場合の確率
-double getNumOfCombination (int N, int X, int Y, int horizon) {
-    int vertical = N-horizon;
+double getNumOfCombination (long N, long X, long Y, long horizon) {
+    long vertical = N-horizon;
     if ((horizon-X)%2 != 0) { return 0;}
     if ((vertical-Y)%2 != 0) { return 0;}
-    int right = X+(horizon-X)/2;
-    int up = Y+(vertical-Y)/2;
+    long right = X+(horizon-X)/2;
+    long up = Y+(vertical-Y)/2;
     if (right<0 || up<0) { return 0;}
     // 確率
     double p = 1;
@@ -34,26 +34,23 @@ double getNumOfCombination (int N, int X, int Y, int horizon) {
 }
 
 // 判定
-double Jump (int N, int D, int X, int Y) {
+double Jump (long N, long D, long X, long Y) {
     double probability = 0;
     if (X%D!=0 || Y%D!=0) {
         probability = 0;
         return probability;
     }
-    int nx = X/D;
-    int ny = Y/D;
+    long nx = X/D;
+    long ny = Y/D;
     if (nx+ny > N) {
         probability = 0;
         return probability;
     }
-    for (int horizon = 0; horizon <= N; horizon++) {
-
+    for (long horizon = 0; horizon <= N; horizon++) {
+        probability += getNumOfCombination(N, X, Y, horizon);
     }
-
     return probability;
 }
-
-
 
 int main(int argc, char const *argv[]) {
     // 変数
@@ -62,5 +59,8 @@ int main(int argc, char const *argv[]) {
     // 入力
     scanf("%ld %ld", &N, &D );
     scanf("%ld %ld", &X, &Y );
+    // 出力
+    double ans = Jump(N, D, X, Y);
+    printf("%lf\n", ans);
     return 0;
 }
