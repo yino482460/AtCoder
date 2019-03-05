8ecb02e5f87d352b4f60f7557c57d14e7d63aebd
yino48246 <yino48246@gmail.com>
2019-03-05 17:39:22 +0900

    パスカルの三角形をいじり中

--- ABC011/D_ABC011.c
+++ ABC011/D_ABC011.c
@@ -2,31 +2,41 @@
 #include <string.h>
 #include <stdlib.h>
 // 確率版コンビネーションを計算
-double Combi (long n, long r) {
+double Combi (int n, int r) {
     double combi[n+1][n+1];
+    memset(combi, 0, sizeof(combi));
     // 初期設定
     combi[0][0] = 1;
     // 組み合わせの計算
-    for (long i = 1; i <= n; i++) {
-        for (long j = 0; j <= i; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int  j = 0; j <= i; j++) {
             if (j-1 >= 0) {
-                combi[i][j] = (combi[i-1][j-1]+combi[i-1][j])*0.5;
+                combi[i][j] = (combi[i-1][j-1]+combi[i-1][j])/2;
             } else {
-                combi[i][j] = combi[i-1][j]*0.5;
+                combi[i][j] = combi[i-1][j]/2;
             }
         }
     }
+    // 確認
+    for (size_t i = 0; i <= n; i++) {
+        for (size_t j = 0; j <= n; j++) {
+            printf("%lf ", combi[i][j] );
+        }
+        printf("\n");
+    }
+    //
     double ans = combi[n][r];
+    printf("p %lf \n", ans );
     return ans;
 }
 
 // 各場合の確率
 double getNumOfCombination (int N, int nx, int ny, int horizon) {
-    long vertical = N-horizon;
-    if ((horizon-nx)%2 != 0) { return 0; }
-    if ((vertical-ny)%2 != 0) { return 0; }
-    long right = nx+(horizon-nx)/2;
-    long up = ny+(vertical-ny)/2;
+    int vertical = N-horizon;
+    if ((horizon+nx)%2 != 0) { return 0; }
+    if ((vertical+ny)%2 != 0) { return 0; }
+    int right = (horizon+nx)/2;
+    int up = (vertical+ny)/2;
     if (right<0 || up<0) { return 0;}
     // 確率
     double p = 0;
