83537210f539241ab2c20926eca5f9d6e39edf6f
yino48246 <yino48246@gmail.com>
2019-03-08 01:06:48 +0900

    無駄に大きいお金の制限を設定していないといけない

--- ABC013/C_ABC013.c
+++ ABC013/C_ABC013.c
@@ -2,13 +2,13 @@
 #define max(a, b) (a>b ? a:b)
 #define min(a, b) (a<b ? a:b)
 // 食いだめ
-int Eat (int N, int H, int A, int B, int C, int D, int E) {
-    int money = 10000000;
+long Eat (long N, long H, long A, long B, long C, long D, long E) {
+    long money = 10000000000000;
     // 探索
-    for (int normal = 0; normal <= N; normal++) {
-        int fasting = N-normal; // 断食日数
-        int satisfy = H+B*normal;   // 満腹度
-        int simple = max((E*fasting+D+E-satisfy)/(D+E), 0);
+    for (long normal = 0; normal <= N; normal++) {
+        long fasting = N-normal; // 断食日数
+        long satisfy = H+B*normal;   // 満腹度
+        long simple = max((E*fasting+D+E-satisfy)/(D+E), 0);
         money = min(money, A*normal+C*simple);
     }
     return money;
@@ -16,14 +16,14 @@ int Eat (int N, int H, int A, int B, int C, int D, int E) {
 
 int main(int argc, char const *argv[]) {
     // 変数
-    int N, H;
-    int A, B, C, D, E;
+    long N, H;
+    long A, B, C, D, E;
     // 入力
-    scanf("%d %d", &N, &H);
-    scanf("%d %d %d %d %d", &A, &B, &C, &D, &E);
+    scanf("%ld %ld", &N, &H);
+    scanf("%ld %ld %ld %ld %ld", &A, &B, &C, &D, &E);
     // 計算
-    int ans = Eat(N, H, A, B, C, D, E);
+    long ans = Eat(N, H, A, B, C, D, E);
     // 出力pr
-    printf("%d\n", ans );
+    printf("%ld\n", ans );
     return 0;
 }
