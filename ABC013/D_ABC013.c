00c59110141117bc5920fc396f2c21a50dcf5ff4
yino48246 <yino48246@gmail.com>
2019-03-08 18:40:02 +0900

    cpp初作成

--- ABC013/D_ABC013.c
+++ ABC013/D_ABC013.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define sizeA 200001
 // 横に移動(本質的にはSwap)
 void Move(int a[], int b[]) {
     int tmp;
@@ -7,6 +8,12 @@ void Move(int a[], int b[]) {
 
 int main(int argc, char const *argv[]) {
     // 変数
-    
+    long N, M, D;
+    int A[sizeA];
+    // 入力
+    scanf("%ld %ld %ld", &N, &M, &D);
+    for (size_t i = 0; i < M; i++) {
+        scanf("%d", &A[i]);
+    }
     return 0;
 }
---
+++ ABC013/D_ABC013.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Amida {
+private:
+    /* data */
+
+public:
+    Amida (arguments);
+    virtual ~Amida ();
+};
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    return 0;
+}
