d14c2d274c62bea074ad13a80031beae86600b14
yino48246 <yino48246@gmail.com>
2019-03-08 18:58:16 +0900

    cppの阿弥陀クラスを実装開始

--- ABC013/D_ABC013.cpp
+++ ABC013/D_ABC013.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <algorithm>
 
 class Amida {
 private:
     /* data */
-
 public:
-    Amida (arguments);
+    int N, M, D;
+    std::vector<int> A;  // 阿弥陀の横線の組み合わせ
+    std::vector<int> toA;    // 移動後の位置
+    Amida ();   // 入力を受け取って配列に設定
     virtual ~Amida ();
+    void move();    // 阿弥陀を移動する
+    void  setResultAmida(); // 答えを返す
 };
+
+// 入力を設定
+Amida :: Amida () {
+    std::cin >> N >> M >> D;    // 入力
+    // 配列に阿弥陀の横棒を設定
+    for (size_t i = 0; i < M; i++) {
+        int tmp;
+        std::cin >> tmp;
+        A.push_back(tmp);
+    }
+}
 
 int main(int argc, char const *argv[]) {
     // 変数
