f6a94af24d824d4c6e87d72d5c064ac9294ef7db
yino48246 <yino48246@gmail.com>
2019-03-09 01:30:21 +0900

    DPの実装

--- ABC013/D_ABC013.c
+++ ABC013/D_ABC013.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #define sizeN 100002
 #define sizeA 200001
+#define maxLogV 30
 #define Swap(a, b) (a+=b, b=a-b, a=a-b)
 // 阿弥陀の到達位置を調べる
 int Amida (long N, long M, long D, int A[]) {
-    int Amida[sizeN];
-    int toA[sizeN];
+    // D=2^iについてj番目を選ぶとどこに飛ぶかという対応表DP[i][j]
+    int DP[maxLogV][sizeN];
+    int toA[sizeN]; // x番目から始めて何番目に到達するか
     // 初期配置
     for (int i = 1; i <= N; i++) {
-        Amida[i] = i;
+        DP[0][i] = i;
     }
-    // 阿弥陀の答えを計算 D=1
-    for (long d = 0; d < D; d++) {
-        for (long i = 0; i < M; i++) {
-            Swap(Amida[A[i]], Amida[A[i]+1]);
-        }
+    // 阿弥陀の答えを計算 D=1 (i=0)の場合
+    for (long i = 0; i < M; i++) {
+        Swap(DP[0][A[i]], DP[0][A[i]+1]);
     }
-    // 追跡
+    // D=1について左からi番目を選ぶとどこに飛ぶか対応表
     for (size_t i = 1; i <= N; i++) {
-        toA[Amida[i]] = i;
+        toA[DP[0][i]] = i;
     }
+    // DPの計算
+    for (int i = 0; i < maxLogV - 1; i++) {
+		for (int j = 0; j < N; ++j) {
+			DP[i + 1][j] = DP[i][DP[i][j]];
+		}
+	}
+
+
+
     // 出力
     for (size_t i = 1; i <= N; i++) {
         printf("%d\n", toA[i] );
