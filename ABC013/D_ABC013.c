61f886ffbb9be16365834fa17c0eb53dfdb0f994
yino48246 <yino48246@gmail.com>
2019-03-08 16:16:25 +0900

    Move関数実装

---
+++ ABC013/D_ABC013.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// 横に移動(本質的にはSwap)
+void Move(int a[], int b[]) {
+    int tmp;
+    tmp = *a; *a = *b; *b = tmp;
+}
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    
+    return 0;
+}
