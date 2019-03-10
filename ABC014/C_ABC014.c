9ccca073637ce8cd3433ba678241213825d9ac5e
yino48246 <yino48246@gmail.com>
2019-03-11 01:40:34 +0900

    C問題Imos法実装

---
+++ ABC014/C_ABC014.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define size 1000
+#define max(a, b) (a>b ? a:b)
+// Imos法
+int retCostmer(int n, int a[], int b[]) {
+    int costmers = 0;
+    int Imos[size] = {0};
+    // 始点と終点を格納
+    for (size_t i = 0; i < n; i++) {
+        Imos[a[i]] ++;
+        Imos[b[i]+1] --;
+    }
+    // 確認
+    for (size_t i = 0; i < 10; i++) {
+        printf("%d ", Imos[i]);
+    }
+    printf("\n");
+    // 被覆
+    for (int i = 1; i < size; i++) {
+        Imos[i] += Imos[i-1];
+        costmers = max(costmers, Imos[i]);
+    }
+    return costmers;
+}
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    int n;
+    int a[size], b[size];
+    // 入力
+    scanf("%d", &n);
+    for (size_t i = 0; i < n; i++) {
+        scanf("%d %d", &a[i], &b[i]);
+    }
+    // 出力
+    int costmers = retCostmer(n, a, b);
+    printf("%d\n", costmers);
+    return 0;
+}
