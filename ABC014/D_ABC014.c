f884f3c1023f73f3654532858f0fa4b0b3867e7e
yino48246 <yino48246@gmail.com>
2019-03-11 21:53:36 +0900

    D問題辺構造体を実装

--- ABC014/D_ABC014.c
+++ ABC014/D_ABC014.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-int InPut();
-int LCA(int N, int Q, int x[], int y[], int a[], int b[]);
-
-int main(int argc, char const *argv[]) {
-    InPut();
-
-
-
-    return 0;
-}
-
+#define X 0
+#define Y 1
+// 辺を表す構造体
+typedef struct edge {
+    int x;
+    int y;
+} edge;
+// 変数
+int N, Q;
+int *a, *b;
+edge *Edge; //ノード同士の連結
+int root; //ルートの番号
+int *parent; //親のノード番号
+int *depth; //ルートからの深さ
+int *conect;    //あるノードがどれだけのノードと繋がっているか
 // 入力
-int InPut () {
-    // 変数
-    int N, Q;
-    int *x, *y, *a, *b;
+int InPut (void) {
     // 入力
     scanf("%d", &N);
-    x = (int*)malloc(sizeof(int)*N);
-    y = (int*)malloc(sizeof(int)*N);
+    Edge = (edge*)malloc(sizeof(edge)*N);
+    parent = (int*)malloc(sizeof(int)*N);
+    depth = (int*)malloc(sizeof(int)*N);
     for (size_t i = 0; i < N; i++) {
-        scanf("%d %d", &x[i], &y[i]);
+        scanf("%d %d", &Edge[i].x-1, &Edge[i].y-1);
     }
     scanf("%d", &Q);
     a = (int*)malloc(sizeof(int)*Q);
     b = (int*)malloc(sizeof(int)*Q);
     for (size_t i = 0; i < N; i++) {
-        scanf("%d %d", &a[i], &b[i]);
+        scanf("%d %d", &a[i]-1, &b[i]-1);
     }
-    LCA(N, Q, x, y, a, b);
     return 0;
 }
+// 深さ優先探索
+void DFS (int x, int p, int d) {
+    // ノードxの親 = p
+    parent[x] = p;
+    // 根っこ(ルート)からノードxまでの深さ = d
+    depth[x] = d;
+    for (size_t i = 0; i < x; i++) {
+        /* code */
+    }
+
+}
 // LCA (n) バージョン
-int LCA (int N, int Q, int x[], int y[], int a[], int b[]) {
-    
+int LCA (void) {
+
+
+    return 0;
+}
+
+int main(int argc, char const *argv[]) {
+    InPut();    // 入力
+
+
 
     return 0;
 }
--- ABC014/Main.java
+++ ABC014/Main.java
@@ -5,5 +5,129 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 class Main{
+    int N, Q;
+    ArrayList<Integer>[] graph;
+    int root;//ルートの番号
+	int[] parent;//親のノード番号
+	int[] depth;//ルートからの深さ
 
+    public void dfs (int v, int p, int d) {
+        //ノードvの親 = p
+		parent[v] = p;
+		//ルートからノードvまでの深さ = d
+		depth[v] = d;
+        for(int i = 0; i<graph[v].size(); i++){
+			if(graph[v].get(i) != p){
+				dfs(graph[v].get(i),v,d + 1);
+			}
+		}
+    }
+    // rootの初期設定
+    public void init(){
+		dfs(root,-1,0);
+	}
+    //uとvのLCAを求める
+	public int LCA(int u,int v){
+		//uとvそれぞれのrootからの深さを揃える
+		while(depth[u] > depth[v]) u = parent[u];
+		while(depth[v] > depth[u]) v = parent[v];
+		//最小祖先ノードで合流するまで１つ１つ向かっていく
+		while(u != v){
+			u = parent[u];
+			v = parent[v];
+		}
+		return u;
+	}
+    // LCAを解く
+    public void solve() {
+		N = nextInt();
+		graph = new ArrayList[N];
+		root = 0;
+		parent = new int[N];
+		depth = new int[N];
+
+		for(int i = 0;i < N;i++){
+			graph[i] = new ArrayList<Integer>();
+		}
+
+		for(int i = 0;i < N - 1;i++){
+			int x = nextInt() - 1;
+			int y = nextInt() - 1;
+
+			graph[x].add(y);
+			graph[y].add(x);
+		}
+
+		init();
+
+		Q = nextInt();
+		for(int i = 0;i < Q;i++){
+			int a = nextInt() - 1;
+			int b = nextInt() - 1;
+
+			int p = LCA(a,b);
+
+			out.println(depth[a] - depth[p] + depth[b] - depth[p] + 1);
+		}
+	}
+    /* Input */
+	private static final InputStream in = System.in;
+	private static final PrintWriter out = new PrintWriter(System.out);
+	private final byte[] buffer = new byte[2048];
+	private int p = 0;
+	private int buflen = 0;
+
+	private boolean hasNextByte() {
+		if (p < buflen)
+			return true;
+		p = 0;
+		try {
+			buflen = in.read(buffer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (buflen <= 0)
+			return false;
+		return true;
+	}
+
+	public boolean hasNext() {
+		while (hasNextByte() && !isPrint(buffer[p])) {
+			p++;
+		}
+		return hasNextByte();
+	}
+    private boolean isPrint(int ch) {
+		if (ch >= '!' && ch <= '~')
+			return true;
+		return false;
+	}
+
+	private int nextByte() {
+		if (!hasNextByte())
+			return -1;
+		return buffer[p++];
+	}
+
+	public String next() {
+		if (!hasNext())
+			throw new NoSuchElementException();
+		StringBuilder sb = new StringBuilder();
+		int b = -1;
+		while (isPrint((b = nextByte()))) {
+			sb.appendCodePoint(b);
+		}
+		return sb.toString();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+    public long nextLong() {
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble() {
+		return Double.parseDouble(next());
+	}
 }
