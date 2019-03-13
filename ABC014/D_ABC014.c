db064b2a53815be24f83acbecfe94ca61d9ff627
yino48246 <yino48246@gmail.com>
2019-03-13 23:10:59 +0900

    連結状態を表現するのに苦闘

--- ABC014/D_ABC014.c
+++ ABC014/D_ABC014.c
@@ -4,20 +4,19 @@
 // 辺を表す構造体
 typedef struct node_t {
     struct node_t *parent;  // 親ノード, 根 = NULL
-    int nodeNum;  // ノード自身の番号
+    int value;  // ノード自身の値
     int depth;  // ルートからの深さ
     struct node_t *child;   // 最初に連結する子ノード
     struct node_t *next;   // 自分の隣のノード 単独ならNULL
 } node_t;
 // ノードを追加する関数
-void addNode (int NodeNum, node_t *parent, node_t *newnode) {
+void addNode (node_t *parent, node_t *newnode, int nodeValue) {
     // 新規ノードのメモリを確保
-    //node_t *node = (node_t*)malloc(sizeof(node_t));
-    newnode -> nodeNum = NodeNum;   // ノードの番号
-    newnode -> depth = (parent -> depth) +1;    // ルートからの深さ
+    newnode -> value = nodeValue;
+    newnode -> depth = (parent -> depth) +1;
     newnode -> parent = parent;    // 新規追加ノードの親ノード
-    newnode -> child = NULL;
     newnode -> next = NULL;    // 追加直後は右隣は無し
+    newnode -> child = NULL;
     // 親ノードから見た新規ノードとの関係
     if (parent -> child == '\0') {
         parent -> child = newnode;
@@ -28,10 +27,17 @@ void addNode (int NodeNum, node_t *parent, node_t *newnode) {
         }
         last -> next = newnode;
     }
-
+}
+// 木構造を構築する関数
+void  MakeTree (int node1, int node2, node_t tree[]) {
+    /* code */
 }
 
+
 int main(int argc, char const *argv[]) {
+    int N;
+    scanf("%d", &N);
+    node_t *nodes = (node_t*)malloc(sizeof(node_t)*N);
 
 
 
