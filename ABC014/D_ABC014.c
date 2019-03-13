402dcab5c3ee31e08b2d9db3170b3b5e1031621b
yino48246 <yino48246@gmail.com>
2019-03-13 14:59:26 +0900

    addNode関数追加

--- ABC014/D_ABC014.c
+++ ABC014/D_ABC014.c
@@ -10,15 +10,24 @@ typedef struct node_t {
     struct node_t *next;   // 自分の隣のノード 単独ならNULL
 } node_t;
 // ノードを追加する関数
-void addNode (int NodeNum, node_t *parent) {
+void addNode (int NodeNum, node_t *parent, node_t *newnode) {
     // 新規ノードのメモリを確保
-    node_t *node = (node_t*)malloc(sizeof(node_t));
-    node -> nodeNum = NodeNum;
-    node -> depth = (parent -> depth) +1;
-    node -> parent = parent;    // 新規追加ノードの親ノード
-    node -> child = NULL;
-    node -> next = NULL;    // 要修正
+    //node_t *node = (node_t*)malloc(sizeof(node_t));
+    newnode -> nodeNum = NodeNum;   // ノードの番号
+    newnode -> depth = (parent -> depth) +1;    // ルートからの深さ
+    newnode -> parent = parent;    // 新規追加ノードの親ノード
+    newnode -> child = NULL;
+    newnode -> next = NULL;    // 追加直後は右隣は無し
     // 親ノードから見た新規ノードとの関係
+    if (parent -> child == '\0') {
+        parent -> child = newnode;
+    } else {
+        node_t *last = parent -> child;
+        while (last -> next == '\0') {
+            last = last -> next;    // 隣に移動
+        }
+        last -> next = newnode;
+    }
 
 }
 
