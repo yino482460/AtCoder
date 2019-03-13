93aa82aeca4d8795c3848ad293b7e5c74a3684b4
yino48246 <yino48246@gmail.com>
2019-03-13 14:46:49 +0900

    node構造体作成開始

--- ABC014/D_ABC014.c
+++ ABC014/D_ABC014.c
@@ -2,72 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 // 辺を表す構造体
-typedef struct edge {
-    int x;
-    int y;
-} edge;
-// 変数
-int N, Q;
-int *a, *b;
-edge *Edge; //ノード同士の連結
-int root; //ルートの番号
-int *parent; //親のノード番号
-int *depth; //ルートからの深さ
-int *conect;    //あるノードがどれだけのノードと繋がっているか
-int **tree; // 木構造を保存
-// 入力
-int InPut (void) {
-    scanf("%d", &N);
-    Edge = (edge*)malloc(sizeof(edge)*N);
-    parent = (int*)malloc(sizeof(int)*N);
-    depth = (int*)malloc(sizeof(int)*N);
-    conect = (int*)malloc(sizeof(int)*N);
-    root = 0;
-    int X,Y;
-    // ノード同士の連結状態を記憶
-    for (int i = 0; i < N-1; i++) {
-        scanf("%d %d", &X, &Y);
-        Edge[i].x = X-1; Edge[i].y = Y-1;
-        conect[X]++; conect[Y]++;
-    }
-    // 木構造を構築する配列を用意
-    tree = (int**)malloc(sizeof(int*)*N);
-    for(int i = 0; i < N; i++){
-        tree[i]=(int*)malloc(sizeof(int)*conect[i]);
-    }
-    // 木構造を構築
-    for (size_t i = 0; i < N-1; i++) {
-        tree[Edge[i].x][i] = Edge[i].y; // バグ
-    }
-    // 追加する辺
-    scanf("%d", &Q);
-    a = (int*)malloc(sizeof(int)*Q);
-    b = (int*)malloc(sizeof(int)*Q);
-    for (size_t i = 0; i < Q; i++) {
-        scanf("%d %d", &a[i]-1, &b[i]-1);
-    }
-    return 0;
-}
-// 深さ優先探索
-void DFS (int x, int p, int d) {
-    // ノードxの親 = p
-    parent[x] = p;
-    // 根っこ(ルート)からノードxまでの深さ = d
-    depth[x] = d;
-    for (size_t i = 0; i < conect[x]; i++) {
-        /* code */
-    }
+typedef struct node_t {
+    struct node_t *parent;  // 親ノード, 根 = NULL
+    int nodeNum;  // ノード自身の番号
+    int depth;  // ルートからの深さ
+    struct node_t *child;   // 最初に連結する子ノード
+    struct node_t *next;   // 自分の隣のノード 単独ならNULL
+} node_t;
+// ノードを追加する関数
+void addNode (int NodeNum, node_t *parent) {
+    // 新規ノードのメモリを確保
+    node_t *node = (node_t*)malloc(sizeof(node_t));
+    node -> nodeNum = NodeNum;
+    node -> depth = (parent -> depth) +1;
+    node -> parent = parent;    // 新規追加ノードの親ノード
+    node -> child = NULL;
+    node -> next = NULL;    // 要修正
+    // 親ノードから見た新規ノードとの関係
 
-}
-// LCA (n) バージョン
-int LCA (void) {
-
-
-    return 0;
 }
 
 int main(int argc, char const *argv[]) {
-    InPut();    // 入力
 
 
 
