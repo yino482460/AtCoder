6a3baaf210c6b5bc03538313bf33ca7d74ae2a30
yino48246 <yino48246@gmail.com>
2019-03-15 00:41:27 +0900

    一旦保存

--- ABC014/D_ABC014.c
+++ ABC014/D_ABC014.c
@@ -7,17 +7,17 @@ typedef struct list_t {
     int node;   // 連結しているノードの値
     struct list_t *previous;    // 1つ前のノード
 } list_t;
-// ノードを表す構造体
-typedef struct node_t {
-    struct node_t *parent;  // 親ノード, 根 = NULL
+// 木を表す構造体
+typedef struct tree_t {
+    struct tree_t *parent;  // 親ノード, 根 = NULL
     int value;  // ノード自身の値
     int depth;  // ルートからの深さ
-    struct node_t *child;   // 最初に連結する子ノード
-    struct node_t *next;   // 自分の隣のノード 単独ならNULL
-} node_t;
+    struct tree_t *child;   // 最初に連結する子ノード
+    struct tree_t *next;   // 自分の隣のノード 単独ならNULL
+} tree_t;
 
 // ノードの初期化
-void InitEdge (int N, list_t node[]) {
+void InitNode (int N, list_t node[]) {
     for (size_t i = 0; i < N; i++) {
         node[i].size = 0;
         node[i].previous = NULL;
@@ -44,9 +44,15 @@ void  strructEdge (int node1, int node2, list_t node[]) {
     conectNode(node1, node2, node);
     conectNode(node2, node1, node);
 }
+// 木構造を構築する関数
+void MakeTree (int v, int p ,int d) {
+    /* code */
+}
+
 
 // ノードを追加する関数
-void addNode (node_t *parent, node_t *newnode, int nodeValue) {
+/*
+void addNode (tree_t *parent, tree_t *newnode, int nodeValue) {
     // 新規ノードのメモリを確保
     newnode -> value = nodeValue;
     newnode -> depth = (parent -> depth) +1;
@@ -57,26 +63,36 @@ void addNode (node_t *parent, node_t *newnode, int nodeValue) {
     if (parent -> child == '\0') {
         parent -> child = newnode;
     } else {
-        node_t *last = parent -> child;
+        tree_t *last = parent -> child;
         while (last -> next == '\0') {
             last = last -> next;    // 隣に移動
         }
         last -> next = newnode;
     }
 }
-
+*/
 
 
 int main(int argc, char const *argv[]) {
     int N;
     scanf("%d", &N);
+    // ノードとその連結状態を構築
     list_t *node = (list_t*)malloc(sizeof(list_t)*N);
-    InitEdge(N, node);
+    InitNode(N, node);
     for (size_t i = 0; i < N-1; i++) {
         int node1, node2;
         scanf("%d %d", &node1, &node2);
         node1 = node1-1; node2 = node2-1;
         strructEdge(node1, node2, node);
+    }
+    // 木を保存する構造体
+    tree_t *tree = (tree_t*)malloc(sizeof(tree_t)*N);
+
+
+    int Q;
+    scanf("%d", &Q);
+    for (size_t i = 0; i < Q; i++) {
+        /* code */
     }
 
 
