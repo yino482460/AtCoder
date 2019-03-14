662ddf9c7b7a2bfa527a3b8b03eb8a667619d284
yino48246 <yino48246@gmail.com>
2019-03-14 21:14:53 +0900

    node構造体関連の関数を実装開始

--- ABC014/D_ABC014.c
+++ ABC014/D_ABC014.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// 可変長ノード構造体
-typedef struct vector {
-    int size;
-
-
-} vec_t;
+// list
+typedef struct list_t {
+    int size;   // 繋がっているノードの数
+    int node;   // 連結しているノードの値
+    struct list_t *next;
+} list_t;
 // ノードを表す構造体
 typedef struct node_t {
     struct node_t *parent;  // 親ノード, 根 = NULL
@@ -15,6 +15,30 @@ typedef struct node_t {
     struct node_t *child;   // 最初に連結する子ノード
     struct node_t *next;   // 自分の隣のノード 単独ならNULL
 } node_t;
+
+// ノードの初期化
+void InitEdge (int N, list_t node[]) {
+    for (size_t i = 0; i < N; i++) {
+        node[i].size = 0;
+        node[i].next = NULL;
+    }
+}
+// ノード同士の連結状態を構築する
+void ConectNode (int node1, int node2, list_t node[]) {
+    list_t *previous, *new;
+    if (node[node1].size == 0) {
+        node[node1].size ++;
+        node[node1].node = node2;
+    } else {
+        new = (list_t*)malloc(sizeof(list_t));  // 新規メモリの確保
+        while (node[node1].next == '\0') {
+
+        }
+        node[node1].next = new;
+    }
+}
+
+
 // ノードを追加する関数
 void addNode (node_t *parent, node_t *newnode, int nodeValue) {
     // 新規ノードのメモリを確保
@@ -34,6 +58,7 @@ void addNode (node_t *parent, node_t *newnode, int nodeValue) {
         last -> next = newnode;
     }
 }
+
 // 木構造を構築する関数
 void  MakeTree (int node1, int node2, node_t node[]) {
     node[node1].value = node1;
@@ -42,16 +67,16 @@ void  MakeTree (int node1, int node2, node_t node[]) {
 
 }
 
-
 int main(int argc, char const *argv[]) {
     int N;
     scanf("%d", &N);
-    node_t *node = (node_t*)malloc(sizeof(node_t)*N);
+    list_t *node = (list_t*)malloc(sizeof(list_t)*N);
+    InitEdge(N, node);
     for (size_t i = 0; i < N-1; i++) {
         int node1, node2;
         scanf("%d %d", &node1, &node2);
         node1 = node1-1; node2 = node2-1;
-        MakeTree(node1, node2, node);
+        ConectNode(node1, node2, node);
     }
 
 
