11f986c5accbb2137771c0b116003e4dfe7d742a
yino48246 <yino48246@gmail.com>
2019-03-16 23:53:49 +0900

    再帰深すぎでエラー

--- ABC015/C_ABC015.c
+++ ABC015/C_ABC015.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#define size 5
+#define size 6
 #define False 0
 #define True 1
 int T[size][size];
+int flag;
 // 深さ優先探索 numQ 今の質問数
-int DFS (int N, int K, int numQ, int value) {
+void DFS (int N, int K, int numQ, int value) {
     if (numQ == N) {
         if (value == 0) {
-            return True;    // バグ有り
+            flag = True;    // バグ有り
         } else {
-            return False;
+            flag = False;
         }
     }
     // 再帰
@@ -17,7 +18,6 @@ int DFS (int N, int K, int numQ, int value) {
         value = value^T[numQ][k];
         DFS(N, K, numQ+1, value);
     }
-    return False;   // バグ無し
 }
 
 int main(int argc, char const *argv[]) {
@@ -30,8 +30,10 @@ int main(int argc, char const *argv[]) {
             scanf("%d", &T[n][k]);
         }
     }
+    // 深さ優先探索
+    DFS(N, K, 0, 0);
     // 出力
-    if (DFS(N, K, 0, 0) == True) {
+    if (flag == True) {
         printf("Found\n");
     } else {
         printf("Nothing\n");
