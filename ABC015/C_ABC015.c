4b9134fd405729597df653cefcbff77b3b50b5f0
yino48246 <yino48246@gmail.com>
2019-03-17 00:47:33 +0900

    デバッグ中

--- ABC015/C_ABC015.c
+++ ABC015/C_ABC015.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-#define size 6
-#define False 0
+#define size 5
 #define True 1
 int T[size][size];
-int cnt = 0;
 int flag;
 // 深さ優先探索 numQ 今の質問数
 void DFS (int N, int K, int numQ, int value) {
-    cnt ++;
-    printf("numQ %d\n", numQ);
     if (numQ == N) {
         if (value == 0) {
-            flag += True;    // バグ有り
-            printf("cnt:%d flag:%d\n",cnt, flag);
-            return;
+            flag = True;    // バグ有り
+            printf("True flag:%d\n", flag);
         } else {
-            flag += False;
-            printf("cnt:%d flag:%d\n",cnt, flag);
-            return;
+            printf("False flag:%d\n", flag);
         }
+        return;
     }
     // 再帰
     for (size_t k = 0; k < K; k++) {
-        value = value^T[numQ][k];
+        value ^= T[numQ][k];
         DFS(N, K, numQ+1, value);
     }
 }
@@ -40,10 +34,10 @@ int main(int argc, char const *argv[]) {
     // 深さ優先探索
     DFS(N, K, 0, 0);
     // 出力
-    if (flag == False) {
-        printf("Nothing\n");
-    } else {
+    if (flag == True) {
         printf("Found\n");
+    } else {
+        printf("Nothing\n");
     }
     return 0;
 }
