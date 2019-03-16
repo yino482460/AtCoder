cd69d940f3c315532d95fdf9fc1834a1327514eb
yino48246 <yino48246@gmail.com>
2019-03-16 13:48:15 +0900

    検証

---
+++ ABC/ABC012/A_ABC012.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    int A, B;
+    // 入力
+    scanf("%d %d", &A, &B );
+    // 出力
+    printf("%d %d\n",B, A);
+    return 0;
+}
