2047fd0aedb30b5680b3f2993d99ab3db5468147
yino48246 <yino48246@gmail.com>
2019-03-17 20:34:06 +0900

    完成!

--- ABC015/D_ABC015.c
+++ ABC015/D_ABC015.c
@@ -4,20 +4,9 @@
 int width[51], importance[51];
 int dp[10001][51][51];  // 動的計画法配列
 int W, N, K;
-// 配列の初期化
-void InitDp( ) {
-    for (size_t i = 0; i < W; i++) {
-        for (size_t j = 0; j < K; j++) {
-            for (size_t k = 0; k < N; k++) {
-                dp[i][j][k] = 0;
-            }
-        }
-    }
-}
 // 動的計画法
 void DP () {
     int maxValue = 0;
-    InitDp();
     // 配列の初期設定
     dp[0][0][0] = 0;
     for (int w = 0; w <= W; w++) {
@@ -25,11 +14,13 @@ void DP () {
             for (int n = 1; n <= N; n++) {
                 int next;
                 if (w-width[n-1] >= 0) {
-                    printf("w:%d k:%d n:%d\n", w, k, n);
+                    //printf("w:%d k:%d n:%d\n", w, k, n);
                     next = dp[w-width[n-1]][k-1][n-1] + importance[n-1];
                     dp[w][k][n] = max(dp[w][k][n-1], next);
                     maxValue = max(dp[w][k][n], maxValue);
-                    printf("dp[w][k][n]:%d maxValue %d\n", dp[w][k][n], maxValue);
+                    //printf("dp[w][k][n]:%d maxValue %d\n", dp[w][k][n], maxValue);
+                } else {
+                    dp[w][k][n] = dp[w][k][n-1];
                 }
             }
         }
