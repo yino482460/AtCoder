ec2179a04d9fdb8dea1dbc9755620f3131eb0146
yino48246 <yino48246@gmail.com>
2019-03-17 13:10:49 +0900

    デバッグ苦しい

--- ABC015/D_ABC015.c
+++ ABC015/D_ABC015.c
@@ -7,8 +7,8 @@ int W, N, K;
 // 配列の初期化
 void InitDp( ) {
     for (size_t i = 0; i < W; i++) {
-        for (size_t j = 0; j < N; j++) {
-            for (size_t k = 0; k < K; k++) {
+        for (size_t j = 0; j < K; j++) {
+            for (size_t k = 0; k < N; k++) {
                 dp[i][j][k] = 0;
             }
         }
@@ -17,18 +17,16 @@ void InitDp( ) {
 // 動的計画法
 void DP () {
     int maxValue = 0;
-    // 配列の初期化
     InitDp();
+    // 配列の初期設定
     dp[0][0][0] = 0;
-    for (int i = 0; i <= W; i++) {
-        for (int j = 1; j <= N; j++) {
-            for (int k = 1; k <= K; k++) {
-                int next = dp[i][j][k];
-                if (i-width[k-1] >= 0) {
-                    printf("call DPif\n");
-                    printf("i-width[k-1] %d\n", i-width[k-1]);
-                    printf("i:%d j:%d k:%d\n", i, j, k-1);
-                    int previous = dp[i-width[k-1]][j-1][k-1] + importance[j-1];
+    for (int w = 0; w <= W; w++) {
+        for (int k = 1; k <= K; k++) {
+            for (int n = 1; n <= N; n++) {
+                if (w-width[n-1] >= 0) {
+                    int next = dp[w][k][n];
+                    printf("w:%d k:%d n:%d\n", w, k, n);
+                    int previous = dp[w-width[n-1]][k-1][n-1] + importance[n-1];
                     next = max(previous, next);
                     maxValue = max(next, maxValue);
                     printf("maxValue %d\n",maxValue);
