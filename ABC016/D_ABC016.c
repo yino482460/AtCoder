e2f8f71499efc8758bfbf370d9d7d5a834a967cb
yino48246 <yino48246@gmail.com>
2019-03-18 01:10:49 +0900

    修正中

--- ABC016/D_ABC016.c
+++ ABC016/D_ABC016.c
@@ -3,17 +3,18 @@
 #define sizeN 101
 int Ax, Ay, Bx, By; // 線分の始点と終点
 int N; // 多角形の頂点数
-// 座標
-typedef struct point {
-    int x;
-    int y;
-} point_t;
+// 位置ベクトル
 typedef struct vec {
-    int vecx;
-    int vecy;
+    int vecx, vecy;
 } vec_t;
+// 外積の計算
+int crossProduct(vec_t a, vec_t b) {
+    int product;
+    product = a.vecx*b.vecy - b.vecx*a.vecy;
+    return product;
+}
 // 線分が交差しているか判定
-int isCross (point_t p[]) {
+int isCross (vec_t start, vec_t end, vec_t p[]) {
     int cross = 0;  // 交差数
     double S1, S2;
     // ベクトル
@@ -21,10 +22,10 @@ int isCross (point_t p[]) {
     base.vecx = Bx-Ax; base.vecy = By-Ay;
     // 交差の判定
     for (size_t i = 0; i < N; i++) {
-        vec1.vecx = p[i].x-Ax;
-        vec1.vecy = p[i].y - Ay;
-        vec2.vecx = p[(i+1)%N].x-Ax;
-        vec2.vecy = p[(i+1)%N].y - Ay;
+        vec1.vecx = p[i].vecx-Ax;
+        vec1.vecy = p[i].vecy - Ay;
+        vec2.vecx = p[(i+1)%N].vecx-Ax;
+        vec2.vecy = p[(i+1)%N].vecy - Ay;
         S1 = 0.5*(base.vecx*vec1.vecy-vec1.vecx*base.vecy);
         S2 = 0.5*(base.vecx*vec2.vecy-vec2.vecx*base.vecy);
         if (S1*S2 < 0) {
@@ -39,16 +40,17 @@ int isCross (point_t p[]) {
 
 int main(int argc, char const *argv[]) {
     // 変数
-    point_t *point;
+    vec_t start, end; // 始点 終点
+    vec_t *point;
     // 入力
-    scanf("%d %d %d %d", &Ax, &Ay, &Bx, &By);
+    scanf("%d %d %d %d", &start.vecx, &start.vecy, &end.vecx, &end.vecy);
     scanf("%d", &N);
-    point = (point_t *)malloc(sizeof(point_t)*(N+1));
+    point = (vec_t *)malloc(sizeof(vec_t)*N);
     for (size_t i = 0; i < N; i++) {
-        scanf("%d %d", &point[i].x, &point[i].y);
+        scanf("%d %d", &point[i].vecx, &point[i].vecy);
     }
     // 出力
-    isCross(point);
+    isCross(start, end, point);
     free(point);
     return 0;
 }
