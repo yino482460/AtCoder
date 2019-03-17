0d68b346228b2ffa54138d171feeeb5c1728e23d
yino48246 <yino48246@gmail.com>
2019-03-17 13:34:05 +0900

    完成?

--- ABC015/D_ABC015.c
+++ ABC015/D_ABC015.c
@@ -23,13 +23,14 @@ void DP () {
     for (int w = 0; w <= W; w++) {
         for (int k = 1; k <= K; k++) {
             for (int n = 1; n <= N; n++) {
+                int next = dp[w][k][n];
                 if (w-width[n-1] >= 0) {
-                    int next = dp[w][k][n];
                     printf("w:%d k:%d n:%d\n", w, k, n);
                     int previous = dp[w-width[n-1]][k-1][n-1] + importance[n-1];
                     next = max(previous, next);
                     maxValue = max(next, maxValue);
-                    printf("maxValue %d\n",maxValue);
+                    dp[w][n][k] = next;
+                    printf("next:%d previous:%d maxValue %d\n",next, previous, maxValue);
                 }
             }
         }
