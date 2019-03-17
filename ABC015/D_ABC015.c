dc011446cc4b2b54a038067b3df85cba7d2d2ded
yino48246 <yino48246@gmail.com>
2019-03-17 12:10:59 +0900

    デバッグ迷走中

--- ABC015/D_ABC015.c
+++ ABC015/D_ABC015.c
@@ -19,17 +19,18 @@ void DP () {
     int maxValue = 0;
     // 配列の初期化
     InitDp();
-    // dp[0][0][0] ={{{0}}};
-    for (int i = 0; i < W; i++) {
-        for (int j = 1; j < N; j++) {
-            for (int k = 1; k < K; k++) {
+    dp[0][0][0] = 0;
+    for (int i = 0; i <= W; i++) {
+        for (int j = 1; j <= N; j++) {
+            for (int k = 1; k <= K; k++) {
                 int next = dp[i][j][k];
-                printf("i:%d j:%d k:%d call DP\n", i, j, k);
-                if (i-width[i-1] > 0) {
-                    printf("i:%d j:%d k:%d call DPif\n", i, j, k);
-                    int previous = dp[i-width[i-1]][j-1][k-1] + importance[k-1];
+                if (i-width[k-1] >= 0) {
+                    printf("call DPif\n");
+                    printf("i-width[k-1] %d\n", i-width[k-1]);
+                    printf("i:%d j:%d k:%d\n", i, j, k);
+                    int previous = dp[i-width[k-1]][j-1][k-1] + importance[k-1];
                     maxValue = max(previous, next);
-                    printf("call DPif2\n");
+                    printf("maxValue %d\n",maxValue);
                 }
             }
         }
@@ -40,7 +41,7 @@ void DP () {
 int main(int argc, char const *argv[]) {
     // 入力
     scanf("%d %d %d", &W, &N, &K);
-    for (size_t i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         scanf("%d %d", &width[i], &importance[i]);
     }
     // 出力
