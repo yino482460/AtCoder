76f906ba612554bd5b84f37b22a02c7e679079a6
yino48246 <yino48246@gmail.com>
2019-03-17 21:00:44 +0900

    動的計画法を省略するのが次の目標

--- ABC015/D_ABC015.c
+++ ABC015/D_ABC015.c
@@ -9,16 +9,14 @@ void DP () {
     int maxValue = 0;
     // 配列の初期設定
     dp[0][0][0] = 0;
+    int next;
     for (int w = 0; w <= W; w++) {
         for (int k = 1; k <= K; k++) {
             for (int n = 1; n <= N; n++) {
-                int next;
                 if (w-width[n-1] >= 0) {
-                    //printf("w:%d k:%d n:%d\n", w, k, n);
                     next = dp[w-width[n-1]][k-1][n-1] + importance[n-1];
                     dp[w][k][n] = max(dp[w][k][n-1], next);
                     maxValue = max(dp[w][k][n], maxValue);
-                    //printf("dp[w][k][n]:%d maxValue %d\n", dp[w][k][n], maxValue);
                 } else {
                     dp[w][k][n] = dp[w][k][n-1];
                 }
