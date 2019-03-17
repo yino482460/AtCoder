3617718ebd31610e424176700fc0f532c8a841dc
yino48246 <yino48246@gmail.com>
2019-03-17 22:08:33 +0900

    WFmethodうろ覚えで適当に実装

--- ABC016/C_ABC016.c
+++ ABC016/C_ABC016.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #define sizeN 11
 int Friend[sizeN][sizeN];
+// ワーシャルフロイド法
+void WFmethod (int N) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            for (size_t k = 0; k < N; k++) {
+                if (i == j) {
+                    Friend[i][j] = 0;
+                } else {
+                    Friend[i][j] = Friend[i][k]+Friend[k][j];
+                }
+            }
+        }
+    }
+    // 確認
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            printf("%2d ", Friend[i][j]);
+        }
+        printf("\n");
+    }
+    // 出力
+    for (size_t i = 0; i < N; i++) {
+        int cnt = 0;
+        for (size_t j = 0; j < N; j++) {
+            if (Friend[i][j] == 2) {
+                cnt ++;
+            }
+        }
+        printf("%d\n", cnt);
+    }
+}
 
 int main(int argc, char const *argv[]) {
     // 変数
@@ -15,6 +46,7 @@ int main(int argc, char const *argv[]) {
         Friend[a][b] = 1;
         Friend[b][a] = Friend[a][b];
     }
-
+    // ワーシャルフロイド
+    WFmethod(N);
     return 0;
 }
