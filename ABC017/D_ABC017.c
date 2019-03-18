5c1fa7c463497141c96282e98b82a0f22216bbfb
yino48246 <yino48246@gmail.com>
2019-03-18 21:36:33 +0900

    苦戦中

--- ABC017/D_ABC017.c
+++ ABC017/D_ABC017.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define sizeF 100001
+#define sizeF 111111
 // 二分累乗
 long lPow(int n, int m) {
     if (m == 0) {
@@ -10,15 +10,22 @@ long lPow(int n, int m) {
         return n*lPow(n, m-1);
     }
 }
-
+// 動的計画法
+void sapliDP (int N, int M, int flaver[]) {
+    int dp[sizeF], sum[sizeF];
+    long Mod = lPow(10, 8) + 7; // 割り算
+    int  left = 0;
+}
+// メイン
 int main(int argc, char const *argv[]) {
-    // 変数
+    // N:サプリの数 M:味の数
     int N, M;
     int flaver[sizeF];
     // 入力
     scanf("%d %d", &N, &M);
     for (size_t i = 0; i < N; i++) {
         scanf("%d", &flaver[i]);
+        flaver[i]--;    // いる?
     }
     return 0;
 }
