908ab28f36fccffe6b82e802aa9f5e5feaaab47f
yino48246 <yino48246@gmail.com>
2019-03-18 11:15:37 +0900

    B問題作成

---
+++ ABC017/B_ABC017.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#define sizeS 51
+// 判定
+void JudgeChoku (char str[]) {
+    int len = strlen(str);
+    for (size_t i = 0; i < len; i++) {
+        if (str[i]!='c' && str[i]!='h' && str[i]!='o' && str[i]!='k' && str[i]!='u') {
+            printf("NO\n");
+            return;
+        } else if ((str[i]=='c' && str[i+1]!='h') || (str[i-1]=='c' && str[i]=='h')) {
+            printf("NO\n");
+            return;
+        } else if (i == len-1) {
+            printf("YES\n");
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    char S[sizeS];
+    // 入力
+    scanf("%s", S);
+    // 出力
+    JudgeChoku(S);
+    return 0;
+}
