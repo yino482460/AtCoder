bad4c22bb1ceb4e59b7214d3540af582a08c7812
yino48246 <yino48246@gmail.com>
2019-03-19 22:17:13 +0900

    重複しないところをカウントする部分でバグ

--- ABC019/C_ABC019.c
+++ ABC019/C_ABC019.c
@@ -9,7 +9,7 @@ void quicksort (int left, int right, long a[]) {
     while (1) {
         while (a[i]  > pivot ) { i++; }
         while (a[j] < pivot ) { j--; }
-        if (i >= j) {
+        if (i >= j) {   // 終了条件
             break;
         }
         swap(a[i], a[j]);
@@ -22,6 +22,46 @@ void quicksort (int left, int right, long a[]) {
         quicksort(j+1, right, a);
     }
 }
+// 奇数になるまで割り算実行
+long divToOdd (long num) {
+    while (num%2 == 0) {
+        num = num/2;
+    }
+    return num;
+}
+// 重複しない数をカウント
+void countUnipue (int N, long a[]) {
+    long cnt = 0;
+    // 数列を降順にソート
+    quicksort(0, N-1, a);
+    for (size_t i = 0; i < N; i++) {
+        a[i] = divToOdd(a[i]);
+    }
+    // デバッグ用
+    for (size_t i = 0; i < N; i++) {
+        printf("%2ld", a[i]);
+    }
+    printf("\n");
+    printf("call countUnipue\n");
+    /*
+    // 重複していない数をカウント
+    int left = 0, right = left;
+    while (1) {
+        if (a[left] != a[right]) {
+            cnt++;
+        }
+        while (a[left] == a[right]) {
+            right ++;
+        }
+
+        left = right;
+        if (right == N) {
+            break;
+        }
+    }
+    */
+    printf("%ld\n", cnt);
+}
 
 int main(int argc, char const *argv[]) {
     // 変数
@@ -33,13 +73,7 @@ int main(int argc, char const *argv[]) {
     for (size_t i = 0; i < N; i++) {
         scanf("%ld", &a[i]);
     }
-    // 計算
-    // 確認
-    quicksort(0, N-1, a);
-    for (size_t i = 0; i < N; i++) {
-        printf("%3ld", a[i]);
-    }
-    printf("\n");
-
+    // 処理
+    countUnipue(N, a);
     return 0;
 }
