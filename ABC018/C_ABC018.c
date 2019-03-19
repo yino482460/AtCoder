14ca2f4646428f93e0bc751c6ac1fe8029e11a63
yino48246 <yino48246@gmail.com>
2019-03-19 12:50:52 +0900

    盤面を数字に変換する関数を実装

--- ABC018/C_ABC018.c
+++ ABC018/C_ABC018.c
@@ -2,19 +2,46 @@
 #include <stdlib.h>
 #define Rmax 500
 #define sizeC 500
+// 色の数字
+enum {
+    white,
+    black,
+    green,
+};
+// 盤面を構築
+void structBord (int R, int C, char **s, int **board) {
+    for (size_t i = 0; i < R; i++) {
+        for (size_t j = 0; j < C; j++) {
+            if (s[i][j] == 'o') {   // 白いマス
+                board[i][j] = white;
+            } else if (s[i][j] == 'x') {
+                board[i][j] = black;
+            }
+        }
+    }
+
+}
 
 int main(int argc, char const *argv[]) {
     // 変数
     int R, C, K;
     char **s;
+    int **board;
     // 入力
     scanf("%d %d %d", &R, &C, &K);
     // 文字列の領域を確保
     s = (char **)malloc(sizeof(char *)*R);
+    board = (int **)malloc(sizeof(int *)*R);
     for (size_t i = 0; i < R; i++) {
         s[i] = (char *)malloc(sizeof(char)*(C+1));
+        board[i] = (int *)malloc(sizeof(int)*C);
         scanf("%s", s[i]);  // 文字列を取得
     }
 
+    // メモリ解放
+    for (size_t i = 0; i < R; i++) {
+        free(s[i]);
+        free(board[i]);
+    }
     return 0;
 }
