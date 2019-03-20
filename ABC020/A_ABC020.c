f840b97578f260f275039038612407000e97e8c6
yino48246 <yino48246@gmail.com>
2019-03-20 17:11:08 +0900

    A問題完成

---
+++ ABC020/A_ABC020.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    int Q;
+    // 入力
+    scanf("%d", &Q);
+    // 出力
+    if (Q == 1) {
+        printf("ABC\n");
+    } else {
+        printf("chokudai\n");
+    }
+    return 0;
+}
