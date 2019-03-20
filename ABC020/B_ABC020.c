d2f8e1568e685d344c13cba73b4f14c028c6685b
yino48246 <yino48246@gmail.com>
2019-03-20 17:38:07 +0900

    B問題のデバッグ開始

---
+++ ABC020/B_ABC020.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#define Sizechar 4
+// 文字列を数字に変換
+void charToDigit (char A[], char B[]) {
+    int digit = 0, num[2*Sizechar];
+    int lena, lenb;
+    lena = strlen(A); lenb = strlen(B);
+    // 文字列を数字に変換
+    for (size_t i = 0; i < lena; i++) {
+        num[i] = A[i] - '0';
+    }
+    for (size_t i = 0; i < lenb; i++) {
+        num[lena+i] = B[i] - '0';
+    }
+    printf("num[1] %d\n", num[1]);
+    // デバッグ
+    for (size_t i = 0; i < (lena+lenb); i++) {
+        printf("%d", num[i]);
+    }
+    printf("\n");
+    // 数値にする
+    for (size_t i = 0; i < (lena+lenb); i++) {
+        digit = digit*10 + num[i];
+    }
+    digit = 2*digit;
+    printf("%d\n", digit);
+}
+
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    char A[Sizechar], B[Sizechar];
+    // 入力
+    scanf("%s %s", A, B);
+    // 出力
+    charToDigit(A, B);
+    return 0;
+}
