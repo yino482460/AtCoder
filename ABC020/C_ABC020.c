66bcd5cc9cb6565963bf694f5244816b0dfd72d4
yino48246 <yino48246@gmail.com>
2019-03-21 17:06:04 +0900

    ダイストラクタ法実装(仮)

--- ABC020/C_ABC020.c
+++ ABC020/C_ABC020.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 int H, W, T;
 int sx, sy, gx, gy;
+int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
 // 座標の情報
 typedef struct point {
     long x, y;
@@ -12,21 +13,38 @@ typedef struct heap {
     int exist; // 配列に値があれば 1
     int distance;
 } heap_t;
+// 関数定義
 long lPow(int n, int m);    // 二分累乗
-void setBoard (char **s, long **dist);    // 文字を数字に変換
-void swapNode (heap_t *a, heap_t *b);   // ノードを交換
-void  addNode (int x, heap_t newnode, heap_t node[]);   // 配列に追加
-void deleteNode (int x, heap_t node[]); // 配列から削除
-void pushHeap (heap_t newnode, heap_t node[]);  // ヒープ化された配列に要素を追加
+void setBoard (char **s, long **dist);    // 文字を整理
+void swapQue (heap_t *a, heap_t *b);   // ノードを交換
+void InitDist(int H, int W, long **dist);   // 距離の初期化
+void InitQue(int H, int W, heap_t que[]);   // キューの初期化
+void  addQue (int x, int y, int n, long **dist, heap_t node[]);   // 配列に追加
+void deleteQue (int x, heap_t node[]); // 配列から削除
+void pushHeap (int x, int y, long **dist, heap_t node[]);  // ヒープ化された配列に要素を追加
 heap_t popHeap (heap_t node[]); // ヒープから最小値を取り出し削除する
+long Dijkstra (int cost, long **dist);  // ダイストラクタ法
+long BinarySearch();    // 二分検索
 
 // ダイストラクタ法
 long Dijkstra (int cost, long **dist) {
     heap_t *que;
     que = (heap_t *)malloc(sizeof(heap_t)*H*W);
-    que[0].P.x = sx; que[0].P.y = sy;
-    que[0].distance = dist[sy][sx];
-    que[0].exist = 1;
+    InitQue(H, W, que);
+    addQue(sx, sy, 0, dist, que);
+    // 本体
+    while (que[0].exist != 0) {
+        heap_t buf = popHeap(que);
+        int x = buf.P.x, y = buf.P.y;
+        for (size_t i = 0; i < 4; i++) {
+            int nx = x+dx[i], ny = y+dy[i];
+            if (ny < 0 || ny >= H || nx < 0 || nx >= W) { continue; }
+            if (dist[ny][nx] > dist[y][x] + cost) {
+            dist[ny][nx] = dist[y][x] + cost;
+            pushHeap(nx, ny, dist, que);
+         }
+        }
+    }
 
     // メモリ解放
     free(que);
@@ -72,56 +90,71 @@ long lPow(int n, int m) {
         return n*lPow(n, m-1);
     }
 }
-// 文字を数字に変換
+// 文字整理
 void setBoard (char **s, long **dist) {
-    // 各マスの距離を無限に設定
-    long INF = lPow(10, 10);
     for (size_t i = 0; i < H; i++) {
         for (size_t j = 0; j < (W+1); j++) {
             scanf("%s", &s[i][j]);
-            dist[i][j] = INF;
             if (s[i][j] == 'S') {
-                sx = j, sy = i;
+                sx = j, sy = i; // スタートの座標を記憶
                 s[i][j] = '.';
             }
             if (s[i][j] == 'G') {
-                gx = j, gy = i;
+                gx = j, gy = i; // ゴールの座標を記憶
                 s[i][j] = '.';
             }
         }
     }
-    //  スタート地点は0
-    dist[sy][sx] = 0;
 }
 // ノードを交換
-void swapNode (heap_t *a, heap_t *b) {
+void swapQue (heap_t *a, heap_t *b) {
     heap_t tmp;
     tmp = *a;
     *a = *b;
     *b = tmp;
 }
-// 配列に追加
-void  addNode (int x, heap_t newnode, heap_t node[]) {
-    node[x] = newnode;
-    node[x].exist = 1;  // 存在
+// 距離の初期化
+void InitDist(int H, int W, long **dist) {
+    // 各マスの距離を無限に設定
+    long INF = lPow(10, 10);
+    for (size_t i = 0; i < H; i++) {
+        for (size_t j = 0; j < W; j++) {
+            dist[i][j] = INF;
+        }
+    }
+    //  スタート地点は0
+    dist[sy][sx] = 0;
 }
-// 配列から削除
-void deleteNode (int x, heap_t node[]) {
+// キューの初期化
+void InitQue(int H, int W, heap_t que[]) {
+    for (size_t i = 0; i < H*W; i++) {
+        que[i].exist = 0;
+    }
+}
+// 配列に追加 n番目のキューに座標 x,yの距離を追加
+void  addQue (int x, int y, int n, long **dist, heap_t node[]) {
+    node[n].P.x = x;
+    node[n].P.y = y;
+    node[n].distance = dist[y][x];
+    node[n].exist = 1;  // 存在
+}
+// x番目のキューから削除
+void deleteQue (int x, heap_t node[]) {
     node[x].exist = 0;
 }
 // ヒープ化された配列に要素を追加
-void pushHeap (heap_t newnode, heap_t node[]) {
+void pushHeap (int x, int y, long **dist, heap_t node[]) {
     int n  = 0; // データ数
     while (node[n].exist != 0) {    // 配列の末尾を探す
         n++;
     }
-    addNode(n, newnode, node);
+    addQue(x, y, n, dist, node);
     // 新規ノードが条件を満たすまで
     while (n != 0) {
         int parent = (n-1)/2; // 親の番号
         // 距離が小さくなるように親と子を入れ替え
         if (node[parent].distance > node[n].distance) {
-            swapNode(&node[n], &node[parent]);
+            swapQue(&node[n], &node[parent]);
         }
         n = parent;
     }
@@ -135,7 +168,7 @@ heap_t popHeap (heap_t node[]) {
     }
     int last = n-1;
     pop = node[last];
-    swapNode(&node[0], &node[last]);
+    swapQue(&node[0], &node[last]);
     for (int i = 0, child; (child=2*i+1) < n; i++) {
         // 左と右を比較して右が小さければ右を上に
         if ((child != n-1) && (node[child].distance > node[child+1].distance)) {
@@ -143,10 +176,10 @@ heap_t popHeap (heap_t node[]) {
         }
         // 子と値を入れ替え
         if (node[i].distance > node[child].distance) {
-            swapNode(&node[i], &node[child]);
+            swapQue(&node[i], &node[child]);
         }
         i = child;
     }
-    deleteNode(last, node);
+    deleteQue(last, node);
     return pop;
 }
