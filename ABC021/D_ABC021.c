e7b9f8df6791aae87e4d52e34de106ca0c4c23c5
yino48246 <yino48246@gmail.com>
2019-03-24 16:44:36 +0900

    時間かかり過ぎ

--- ABC021/D_ABC021.c
+++ ABC021/D_ABC021.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // 二分累乗
-long lPow (int n, int m) {
+long lPow (long n, long m) {
     if (m == 0) {
         return 1;
     } else if (m == 1) {
@@ -11,6 +12,41 @@ long lPow (int n, int m) {
     } else {
         return n*lPow(n, m-1);
     }
+    printf("call lPow\n");
+}
+// 階乗の計算
+void lFactrial (int n, long Mod, long *factrial) {
+    if (n == 0) {
+        factrial[0] = 1;
+    }
+    for (size_t i = 0; i <= n; i++) {
+        factrial[i] = (i*factrial[i-1])%Mod;
+    }
+    printf("call lFactrial\n");
+}
+// 逆元の計算
+void Inverse (int n, long Mod, long *factrial, long *inverse) {
+    inverse[n] = lPow(factrial[n], (Mod-2))%Mod;     // フェルマーの小定理
+    printf("call Inverse\n");
+}
+// nHkの計算
+void  calc_nHk (int n, int k) {
+    long Mod = lPow(10, 9)+7;
+    long nHk;
+    long *factrial, *inverse;
+    factrial = (long *)malloc(sizeof(long)*(n+k));
+    inverse = (long *)malloc(sizeof(long)*(n+k));
+    lFactrial(n, Mod, factrial);
+    //printf("call calc_nHk\n");
+    Inverse(n, Mod, factrial, inverse);
+    printf("call calc_nHk\n");
+    // nHkの計算
+    int N = n+k-1, K = n-1;
+    nHk = factrial[N]*inverse[K]%Mod*inverse[N-K];
+    // 出力
+    printf("%ld\n", nHk);
+    // メモリ解放
+    free(factrial); free(inverse);
 }
 
 // メイン
@@ -19,7 +55,10 @@ int main(int argc, char const *argv[]) {
     int n, k;
     // 入力
     scanf("%d %d", &n, &k);
-
-
+    // 階乗
+    long *fact;
+    fact = (long *)malloc(sizeof(long)*n);
+    // 出力
+    calc_nHk(n, k);
     return 0;
 }
