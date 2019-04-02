9162a8b6db7e7156046200e21097b866019ebda9
yino48246 <yino48246@gmail.com>
2019-04-02 23:03:33 +0900

    A問題完成

---
+++ ABC026/A_ABC026.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    // 変数
+    int N;
+    // 入力
+    scanf("%d", &N);
+    // 計算
+    int ans;
+    ans = N*N/4;
+    // 出力
+    printf("%d\n", ans);
+    return 0;
+}
