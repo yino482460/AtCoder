f4e60add64f0710effbf4ac8be320a24011b1527
yino48246 <yino48246@gmail.com>
2019-04-07 22:08:23 +0900

    D問題完成

--- ABC028/D_ABC028.cpp
+++ ABC028/D_ABC028.cpp
@@ -2,10 +2,11 @@
 
 int main(int argc, char* argv[])
 {
-    long long N, K;
+    double N, K;
     std::cin >> N >> K;
     double ans;
     ans = ((N-K)*(K-1)*6 + (N-1)*3 + 1)/(N*N*N);
+    std::cout.precision(15);
     std::cout << ans << "\n";
     return 0;
 }
