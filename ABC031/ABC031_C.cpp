e829e192c104cd56300c41917ebe7fcb379b94f8
yino48246 <yino48246@gmail.com>
2020-11-07 17:29:54 +0900

    C問題完成

--- ABC031/ABC031_C.cpp
+++ ABC031/ABC031_C.cpp
@@ -13,25 +13,86 @@ int main(int argc, char const *argv[])
     cin >> a[i];
   }
 
-  /* 最大値の計算 */
-  int max = INT16_MAX;
-  int T_score = 0;  // 高橋の得点
-  int A_score = 0;  // 青木の得点
-  vector<int> T;
-  for (size_t i = 0; i < N; i++)  // 高橋の選択
+  /* メイン計算 */
+  vector<int> dp(N);   // 累積和の計算
+  dp[0] = a[0];
+  dp[1] = a[1];
+  for (size_t i = 2; i < N; i++)
   {
-    for (size_t j = N-1; j > i; j--)  // 青木の選択
-    {
-      T.push_back(a[i]);
-    }
+    dp[i] = dp[i-2] + a[i];
   }
 
-  for (size_t i = 0; i < T.size(); i++)
+  // 全探索
+  int Takahashi = INT16_MIN;      // 高橋の得点
+  int Aoki = INT16_MIN;           // 青木の得点
+  int ans = INT16_MIN;            // 得点
+  for (int i = 0; i < N; i++)  // 高橋の選択位置
   {
-    cout << T[i] << endl;
+    int Aoki_max = INT16_MIN; // 青木の最高得点
+    int Aoki_max_j = 0; // Takahashiの得点が最大になる位置
+    for (int j = 0; j < N; j++)  // 青木の選択位置
+    {
+      if (i==j)
+      {
+        continue;
+      }
+      int left = min(i,j), right = max(i, j); // 配列Tの両端を設定
+      // 青木の得点の計算
+      if ((right - left)%2 == 0)
+      {
+        if ((left -1) >= 0) {
+          Aoki = dp[right-1] - dp[left-1];
+        } else {
+          Aoki = dp[right-1];
+        }
+      }
+      else
+      {
+        if ((left - 1) >= 0)
+        {
+          Aoki = dp[right] - dp[left-1];
+        }
+        else
+        {
+          Aoki = dp[right];
+        }
+      }
+      // 青木の最高得点を更新
+      if (Aoki > Aoki_max)
+      {
+        Aoki_max = Aoki;
+        Aoki_max_j = j;
+      }
+    }
+
+    int left = min(i, Aoki_max_j), right = max(i, Aoki_max_j); // 配列Tの両端を設定
+    // 高橋の得点の計算
+    if ((right - left) % 2 == 0)
+    {
+      if ((left - 2) >= 0)
+      {
+        Takahashi = dp[right] - dp[left - 2];
+      }
+      else
+      {
+        Takahashi = dp[right];
+      }
+    }
+    else
+    {
+      if ((left - 2) >= 0)
+      {
+        Takahashi = dp[right - 1] - dp[left - 2];
+      }
+      else
+      {
+        Takahashi = dp[right - 1];
+      }
+    }
+    ans = max(ans, Takahashi);
   }
 
   /* 出力 */
-  // cout << max <<endl;
+  cout << ans <<endl;
   return 0;
 }
