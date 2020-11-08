c20f399f2b2e9788035850a2aebe243520b20900
yino48246 <yino48246@gmail.com>
2020-11-08 16:29:33 +0900

    ABC032_D放置

---
+++ ABC032/ABC032_D.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  long N, W;
+  cin>> N>> W;
+  vector<long> n(N), w(N);
+  for (size_t i = 0; i < N; i++)
+  {
+    cin>> n[i]>> w[i];
+  }
+
+  /* 動的計画法 */
+
+
+  return 0;
+}
