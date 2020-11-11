c1566a08a8038f9b1f9e4576240fcc6a94f8f47e
yino48246 <yino48246@gmail.com>
2020-11-11 22:21:58 +0900

    B問題作成中1

--- ABC035/ABC035_B.cpp
+++ ABC035/ABC035_B.cpp
@@ -1,9 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Point
+{
+  int x;
+  int y;
+};
+
+
 int main(int argc, char const *argv[])
 {
   /* 入力 */
+  string S;
+  int T;
+  cin >> S>> T;
+
+  /* 計算 */
+  int unkown = 0; // ?の個数
+  Point p;
+  // 座標の計算
+  for (int i = 0; i < S.size(); i++) {
+    switch (S[i])
+    {
+    case 'L':
+      p.x--;
+      break;
+    case 'R':
+      p.x++;
+      break;
+    case 'U':
+      p.y++;
+      break;
+    case 'D':
+      p.y--;
+      break;
+    default:
+      unkown++;
+      break;
+    }
+  }
+
 
   return 0;
 }
