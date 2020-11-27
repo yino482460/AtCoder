9e9c88f058144193480bbc4a06d39b439b963133
yino48246 <yino48246@gmail.com>
2020-11-27 23:46:17 +0900

    座標圧縮実装

---
+++ ABC036/ABC036_C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  int N, tmp;
+  vector<int> a, b;
+  map<int, int> mp;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> tmp;
+    a.push_back(tmp);
+    b.push_back(tmp);
+  }
+
+  /* 計算 */
+  sort(a.begin(), a.end());
+  int count = 0;
+  for (int i = 0; i < N; i++) {
+    if (mp[a[i]] == 0)
+    {
+      count++;
+      mp[a[i]] = count;
+    }
+  }
+
+  /* 出力 */
+  for (int i = 0; i < N; i++) {
+    cout << mp[b[i]]-1 << endl;
+  }
+
+  return 0;
+}
