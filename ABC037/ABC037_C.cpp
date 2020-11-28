e1c857c120652963cf9eb2cc3aa8f3ec3c3b58a8
yino48246 <yino48246@gmail.com>
2020-11-28 23:15:52 +0900

    C問題デバッグ

---
+++ ABC037/ABC037_C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  int N, K;
+  cin >> N >> K;
+  vector<long long> a(N, 0), S(N, 0);
+  for (int i = 0; i < N; i++) {
+    cin >> a[i];
+    if (i == 0)
+    {
+      S[i] = a[i];
+    }
+    else
+    {
+      S[i] = S[i-1] + a[i];
+    }
+  }
+
+  /* 入力 */
+  long long ans = 0;
+  for (int i = 0; i < N-K+1; i++) {
+    ans += S[i+K] - S[i];
+  }
+
+  /* 出力 */
+  cout << ans << endl;
+
+  return 0;
+}
