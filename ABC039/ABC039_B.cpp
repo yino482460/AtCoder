7cb42ef10dfa9e4b32fa2d698f600e54f20cb362
yino48246 <yino48246@gmail.com>
2020-12-02 23:15:07 +0900

    B問題完成

---
+++ ABC039/ABC039_B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  double X;
+  cin >> X;
+
+  /* 出力 */
+  double ans;
+  ans = pow(X, 0.25);
+  cout << ans << endl;
+
+  return 0;
+}
