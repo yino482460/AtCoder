194113cfb40fbe67009820bcdcfd17aeee7e2538
yino48246 <yino48246@gmail.com>
2020-12-14 20:14:16 +0900

    A問題完成

---
+++ ABC041/ABC041_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  string s;
+  int i;
+  cin >> s >> i;
+
+  /* 出力 */
+  cout << s[i-1] << endl;
+  return 0;
+}
