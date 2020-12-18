a26cd90c41e5091a6b4c8fe22ce07ac729971baa
yino48246 <yino48246@gmail.com>
2020-12-18 16:42:48 +0900

    A問題完成

---
+++ ABC043/ABC043_A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  int N;
+  cin >> N;
+
+  /* 計算 */
+  int ans = N*(N+1)/2;
+
+  /* 出力 */
+  cout << ans << endl;
+
+  return 0;
+}
