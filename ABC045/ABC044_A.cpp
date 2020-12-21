f14aa0a924712d0c04bdbaa9b6ed8746818f6477
yino48246 <yino48246@gmail.com>
2020-12-21 21:54:12 +0900

    A問題完成

---
+++ ABC045/ABC044_A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  int a, b, h;
+  cin >> a >> b >> h;
+
+  /* 計算 */
+  int ans = (a+b)*h/2;
+
+  /* 出力 */
+  cout << ans << endl;
+
+  return 0;
+}
