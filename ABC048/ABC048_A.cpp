742443336add791c487da01ebfcaea0c740514eb
yino48246 <yino48246@gmail.com>
2021-01-06 21:13:15 +0900

    ABC048 A問題完成

---
+++ ABC048/ABC048_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  /* 入力 */
+  string a, b, c;
+  cin >> a >> b >> c;
+
+  /* 出力 */
+  cout << a[0] << b[0] << c[0] << endl;
+
+  return 0;
+}
