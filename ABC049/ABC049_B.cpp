60334ca620d348562d21440acf4e13f0f3200849
yino48246 <yino48246@gmail.com>
2021-01-28 20:56:36 +0900

    B問題完成

--- ABC049/ABC049_B.cpp
+++ ABC049/ABC049_B.cpp
@@ -4,5 +4,22 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
   /* 入力 */
+  int H, W;
+  cin >> H >> W;
+  char C[H][W];
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      cin >> C[i][j];
+    }
+  }
+
+  /* 出力 */
+  for (int i = -1; i < 2*H-1; i++) {
+    for (int j = 0; j < W; j++) {
+      cout << C[(i+1)/2][j];
+    }
+    cout << endl;
+  }
+
   return 0;
 }
